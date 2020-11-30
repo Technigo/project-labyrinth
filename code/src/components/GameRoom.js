0b3377d522815c17dfa8bbbac9dcc6e0b79223ee
Katsiez <katarinajovanovic.a@gmail.com>
2020-11-30 21:23:58 +0100

    trying to get all the game data to display

--- code/src/components/GameRoom.js
+++ code/src/components/GameRoom.js
@@ -5,17 +5,18 @@ import {useDispatch, useSelector} from 'react-redux'
 import {game} from '../reducers/game'
 
 export const GameRoom = () => {
-	//const dispatch = useDispatch()
+
+  const dispatch = useDispatch()
+  
   const gameData = useSelector((store) => store.game.game)
+  const addUsername = useSelector((store) => store.game.name)
+  const array = useSelector((store) => store.game.game.actions)
   
   console.log(gameData)
   
   return (
     <div>
-      {/* <p>{gameData.description}</p>
-	  {gameData.actions.map((item) => (
-      return( <p></p>)
-    })} */}
+    <p>{gameData.description}</p>
   	<button>test</button>
     </div>
   )
