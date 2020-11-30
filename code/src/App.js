56ce1d978d1cb830930b346a55cf2470f27550eb
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-11-30 15:44:12 +0100

    Some Component and CSS Added

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import image from './assets/2792203.jpg'
+import styled from 'styled-components/macro'
 
+import { SelectionButton } from './components/SelectionButtons'
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className='container'>
+      <img className='bodyImage' src={image} alt="pills"/>
+      <div>
+        <SelectionButton type='blue'>Click Me!</SelectionButton>
+        <SelectionButton type='red'>Click Me!</SelectionButton>
+      </div>
+
     </div>
   )
 }
