5fbce09e0e0c259d1405bd4863ed669f8c8dbfb8
Peggy <64912576+pwangy@users.noreply.github.com>
2020-12-01 18:57:16 +0100

    set username and start game work

--- code/src/reducers/game.js
+++ code/src/reducers/game.js
@@ -1,43 +1,68 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quote-props */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-undef */
 import { createSlice } from '@reduxjs/toolkit'
 
 import { ui } from './ui'
 
-const newUser = {
-  username: null,
-  currenStep: {},
+const initialState = {
+  username: '',
+  currentStep: {},
   history: []
 }
 
 export const game = createSlice({
   name: 'game',
-  initialState: newUser,
+  initialState,
   reducers: {
-    startGame: (state, action) => {
-      const { username } = action.payload
-      state.username.push({ username })
+    setUser: (state, action) => {
+      state.username = action.payload
+    },
+    setCurrentStep: (state, action) => {
+      const stateNow = action.payload
+      state.history = [...state.history, state.currentStep]
+      state.currentStep = stateNow
     }
   }
 })
 
-export const fetchStart = () => {
-  const START_API = 'https://wk16-backend.herokuapp.com/start'
+export const fetchStart = (User) => {
+  const START_API = 'https://wk16-backend.herokuapp.com/start/'
+
   return (dispatch) => {
     dispatch(ui.actions.setLoading(true))
     fetch(START_API, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: username })
+      body: JSON.stringify({ username: User })
     })
       .then((res) => res.json())
       .then((json) => {
-        console.log(json)
-        dispatch(game.actions.startGame)
+        dispatch(game.actions.setCurrentStep(json))
         dispatch(ui.actions.setLoading(false))
+        console.log(json)
       })
   }
 }
 
-// reducer to advance to next step
-// nextStep
+export const fetchNext = (User, direction) => {
+  const NEXT_API = 'https://wk16-backend.herokuapp.com/action/'
+
+  return (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch(NEXT_API, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: User, type: "move", direction: direction }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(game.actions.setCurrentStep(json))
+        dispatch(ui.actions.setLoading(false))
+        console.log(json)
+      })
+  }
+}
