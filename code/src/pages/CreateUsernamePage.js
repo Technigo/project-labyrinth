da6104aa2e5f4f8435aec2fd4435ac8077ce3dc3
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-12-02 14:03:05 +0100

    Start styling: Create Username Page done

--- code/src/pages/CreateUsernamePage.js
+++ code/src/pages/CreateUsernamePage.js
@@ -1,8 +1,48 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 
 import { gamestate } from 'reducers/gamestate';
 import { StartGamePage } from './StartGamePage';
+
+const CreateUsernameBackground = styled.div`
+  background-image: url("https://walterbaiamonte.files.wordpress.com/2015/11/cabinet1.gif?w=372");
+  background-size: cover;
+  background-position: center;
+  height: 700px;
+  max-width: 1350px;
+  margin: auto;
+  margin-top: 100px;
+`;
+
+const UsernameFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20%;
+  text-align: center;
+  color: white;
+  max-width: 250px;
+  margin: auto;
+
+  // Mobile query
+  @media (max-width: 768px) {
+    padding-top: 250px;    
+  }
+
+  // Tablet query
+  @media (min-width: 769px) and (max-width: 1024px) {
+    padding-top: 270px;    
+  }
+`;
+
+const UsernameButton = styled.button`
+  background-color: rgb(32, 8, 191);
+  color: white;
+  font-family: 'Press Start 2P', cursive;
+  margin-top: 20px;
+  padding: 20px;
+  cursor: pointer;
+`;
 
 // In this component there is a text input field for the user to enter a username
 // this username is saved in a local state and sent as a prop to our setUsername
@@ -28,20 +68,22 @@ export const CreateUsernamePage = () => {
   };
 
   return (
-    <>
-    <h1>MAZE GAME</h1>
-      <form onSubmit={addUsername}>
-        <label>
-          Enter a Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            />
-        </label>
-        <button type="submit">Create Username</button>
-      </form>
-    </>
+    <CreateUsernameBackground>
+      <UsernameFormContainer>
+        <h1>MAZE GAME</h1>
+          <form onSubmit={addUsername}>
+            <label>
+              Enter a Username:
+              <input
+                type="text"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                />
+            </label>
+            <UsernameButton type="submit">Create Username</UsernameButton>
+          </form>
+      </UsernameFormContainer>
+    </CreateUsernameBackground>
   )
 };
 
