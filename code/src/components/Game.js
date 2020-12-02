0b3822a8b3a51b65f77151e25f62a8ef2303ca66
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-12-02 21:40:28 +0100

    Some Styling added

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -2,14 +2,42 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { 
+	Container,
 	Button,
 	TextField,
-	Card,
-	CircularProgress
+	Box,
+	CircularProgress,
 } from '@material-ui/core'
 import AirplanemodeActiveSharpIcon from '@material-ui/icons/AirplanemodeActiveSharp';
+import styled from 'styled-components'
 
 import { thunk, actionThunk, games } from '../reducers/games'
+
+const Discription = styled.p`
+	font-size: 40px;
+	font-weight: 500;
+	text-align: center;
+`
+const StartForm = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	margin-top: 10%;
+	background: rgba(191, 191, 191, 0.5);
+	padding: 20px 20px 60px 20px;
+	border-radius: 20px;
+`
+
+const GameContainer = styled.div `
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background: rgba(191, 191, 191, 0.5);
+	padding: 20px 20px 60px 20px;
+	border-radius: 20px;
+`
 
 export const Game = () => {
 	const gameDetails = useSelector((store) => store.games)
@@ -30,51 +58,52 @@ export const Game = () => {
 	}
 
 	return (
-		<div className='app-container'>
+		<Container style={{ marginTop: 100, paddingTop: 100, maxWidth: 700 }}>
 			{showStart && (
-				<form className='form' onSubmit={() => onStart()}>
-					<TextField
-						variant='filled' 
-						placeholder="Write a username"
-						color ="secondary"
-						type="text"
-						label="Type Name Here"
-						required
-						onChange={(event) => setNewUserName(event.target.value)}
-						value={newUserName}
-					/>
-					<Button
-						className='button'
-						startIcon = {< AirplanemodeActiveSharpIcon/>}
-						variant="contained" 
-						color="primary"
-						type='submit'>
-						Start game
-					</Button>
-				</form>
+				<StartForm>
+					<Discription>Enter you name to start ...</Discription>
+					<form  onSubmit={() => onStart()}>
+						<TextField
+							variant='filled' 
+							placeholder="Write a username"
+							color ="secondary"
+							type="text"
+							label="Type Name Here"
+							required
+							onChange={(event) => setNewUserName(event.target.value)}
+							value={newUserName}
+						/>
+						<Button
+							style={{ margin: 10, maxWidth: 200 }}
+							startIcon = {< AirplanemodeActiveSharpIcon/>}
+							variant="contained" 
+							color="primary"
+							type='submit'>
+							Start game
+						</Button>
+					</form>
+				</StartForm>
 			)}
 
 			{loader && (
-				<div>
+				<Box display="flex" justifyContent="center">
 					<CircularProgress/>
 					<p>Loading...</p>
-				</div>
+				</Box>
 			)}
 
 			{!showStart && (
-				<div className="discreption-container">
-					<div className='discription'>
-						<p>{gameDetails.description}</p>
-					</div>
+				<GameContainer>
+					<Discription>{gameDetails.description}</Discription>
 					<div>
 						{gameDetails.actions.map((action) => (
-							<Button variant="contained" color="primary" key={action.description} onClick={() => onAction(action)}>
-								{action.type} {action.direction}
-							</Button>
+						<Button variant="contained" color="primary" key={action.description} onClick={() => onAction(action)}>
+							{action.type} {action.direction}
+						</Button>
 						))}
 					</div>
-				</div>
+				</GameContainer>
 			)}
-		</div>
+		</Container>
 	)
 }
