8845165281e0cc59ed0a6aa8b9c3e2c99e3fe7a8
MindstormingAB <s.elander@mindstorming.se>
2020-12-06 19:49:51 +0100

    Removed previous loader

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,25 +1,9 @@
 import React from "react";
-// import styled from "styled-components/macro";
 import Lottie from "react-lottie";
 import { useSelector } from "react-redux";
 
 import { CenteredScreen } from "../lib/Styling";
-// import { Foxy } from "../assets/lottie_fox.json";
 import animationData from "../assets/loader.json";
-
-// const Loader = styled.div`
-//   border: 16px solid #f3f3f3;
-//   border-top: 16px solid #ff6932;
-//   border-radius: 50%;
-//   width: 200px;
-//   height: 200px;
-//   animation: spin 2s linear infinite;
-
-//   @keyframes spin {
-//     0% { transform: rotate(0deg); }
-//     100% { transform: rotate(360deg); }
-//   }
-// `;
 
 export const Loading = () => {
   const isLoading = useSelector((state) => state.ui.isLoading);
