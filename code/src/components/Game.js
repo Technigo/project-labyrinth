b3fb14983bcb69853c0f4403cef933c9f1dbc7e4
AxelHammarback <axel.hammarback@gmail.com>
2020-12-06 16:08:41 +0100

    Made it responsive + added readme

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -9,14 +9,15 @@ import arrow_one from '../assets/arrow-one.svg'
 import arrow_split from '../assets/arrow-split.svg'
 
 export const Game = () => {
-  // Define some base variables
   const username = useSelector(store => store.rooms.username)
   const room = useSelector(store => store.rooms)
   // We'll use this variable to check if things are loading or not.
   const isLoading = useSelector(store => store.loader.isLoading)
   const dispatch = useDispatch();
+
   const START_URL = "https://wk16-backend.herokuapp.com/start";
   const ACTION_URL = "https://wk16-backend.herokuapp.com/action";
+
   const startGameFetchInfo = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -38,7 +39,7 @@ export const Game = () => {
       })
   }
 
-  // This function is called on each click of a "Direction" button – i.e. the buttons in each action box with north, east, west or south.
+  // This function is called on each click of a "Direction" button – i.e. the buttons in each action box with North, East, West or South.
   const continueGame = (direction) => {
     dispatch(loader.actions.setLoading(true))
     fetch(ACTION_URL, {
@@ -71,19 +72,20 @@ export const Game = () => {
         <p>"{room.gameState.description}"</p>
       </div>
 
+      {/* Logic to display either one arrow, two arrows, or a "Game completed" text */}
       {
         (room.gameState.actions.length === 1)
-          ? <img src={arrow_one} alt="arrow" />
+          ? <img className="image-arrow" src={arrow_one} alt="arrow" />
           : null
       }
       {
         (room.gameState.actions.length === 2)
-          ? <img src={arrow_split} alt="arrow" />
+          ? <img className="image-arrow" src={arrow_split} alt="arrow" />
           : null
       }
       {
         (room.gameState.actions.length === 0)
-          ? <><h2 className="gameover-text">Game over!</h2><button onClick={() => window.location.reload()}>&gt; Restart game</button></>
+          ? <><h2 className="gameover-text">You completed the labyrinth!</h2><button onClick={() => window.location.reload()}>&gt; Restart game</button></>
           : null
       }
 
@@ -92,12 +94,11 @@ export const Game = () => {
         {room.gameState.actions.map((action, index) =>
           <div className="box-action">
             <p>"{action.description}"</p>
-            {/* WHAT'S THE KEY? */}
-            {/* When clicking, run the "continueGame" function, and pass the direction as an argument. */}
             <button
               key={action.direction}
-              // If the game is loading (using the global "isLoading" variable), set disabled to true to prevent multiple clicks.
+              /* If the game is loading (using the global "isLoading" variable), set disabled to true to prevent multiple clicks.*/
               disabled={isLoading}
+              /* When clicking button, run the "continueGame" function, and pass the direction as an argument. */
               onClick={() => continueGame(action.direction)}
             >
               {/* &gt; (gt = greater than) is the HTML entity for printing a ">" character, since the buttons have one of those. */}
