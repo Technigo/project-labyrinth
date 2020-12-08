f63612a16c520654f636341c09568a2c7e0e01bb
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-12-08 18:02:05 +0100

    Tabs changed to spaces

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { 
-	Container,
-	Button,
-	TextField,
-	CircularProgress,
-	ButtonGroup
+  Container,
+  Button,
+  TextField,
+  CircularProgress,
+  ButtonGroup
 } from '@material-ui/core'
 import AirplanemodeActiveSharpIcon from '@material-ui/icons/AirplanemodeActiveSharp'
 import styled from 'styled-components'
@@ -14,95 +14,95 @@ import styled from 'styled-components'
 import { thunk, actionThunk, games } from '../reducers/games'
 
 const Discription = styled.p`
-	font-size: 40px;
-	font-weight: 500;
-	text-align: center;
+  font-size: 40px;
+  font-weight: 500;
+  text-align: center;
 `
 
 const Box = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	background: rgba(191, 191, 191, 0.5);
-	padding: 20px 20px 60px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: rgba(191, 191, 191, 0.5);
+  padding: 20px 20px 60px 20px;
 `
 
 export const Game = () => {
-	const gameDetails = useSelector((store) => store.games)
-	const loader = useSelector((store) => store.ui.isLoading)
-	const [newUserName, setNewUserName] = useState('')
-	const [showStart, setShowStart] = useState(true)
+  const gameDetails = useSelector((store) => store.games)
+  const loader = useSelector((store) => store.ui.isLoading)
+  const [newUserName, setNewUserName] = useState('')
+  const [showStart, setShowStart] = useState(true)
 
   const dispatch = useDispatch()
   
-	const onStart = () => {
-		dispatch(thunk(newUserName))
-		dispatch(games.actions.setUsername(newUserName))
-		setShowStart(false)
-	}
+  const onStart = () => {
+    dispatch(thunk(newUserName))
+    dispatch(games.actions.setUsername(newUserName))
+    setShowStart(false)
+  }
 
-	const onAction = (action) => {
-		dispatch(actionThunk(newUserName, action))
-	}
+  const onAction = (action) => {
+    dispatch(actionThunk(newUserName, action))
+  }
 
-	const onHistory = () => {
-		dispatch(games.actions.history())
-	}
+  const onHistory = () => {
+    dispatch(games.actions.history())
+  }
 
-	return (
-		<Container style={{ marginTop: 100, paddingTop: 100, maxWidth: 700 }}>
-			{showStart && (
-				<Box>
-					<Discription>Enter you name to start...</Discription>
-					<form  onSubmit={() => onStart()}>
-						<TextField
-							variant='filled' 
-							placeholder='Write a username'
-							color ='secondary'
-							type='text'
-							label='Type Name Here'
-							required
-							onChange={(event) => setNewUserName(event.target.value)}
-							value={newUserName}
-						/>
-						<Button
-							style={{ margin: 10, maxWidth: 200 }}
-							startIcon = {< AirplanemodeActiveSharpIcon/>}
-							variant='contained'
-							color='primary'
-							type='submit'>
-							Start game
-						</Button>
-					</form>
-				</Box>
-			)}
+  return (
+    <Container style={{ marginTop: 100, paddingTop: 100, maxWidth: 700 }}>
+      {showStart && (
+        <Box>
+          <Discription>Enter you name to start...</Discription>
+          <form  onSubmit={() => onStart()}>
+            <TextField
+              variant='filled' 
+              placeholder='Write a username'
+              color ='secondary'
+              type='text'
+              label='Type Name Here'
+              required
+              onChange={(event) => setNewUserName(event.target.value)}
+              value={newUserName}
+            />
+            <Button
+              style={{ margin: 10, maxWidth: 200 }}
+              startIcon = {< AirplanemodeActiveSharpIcon/>}
+              variant='contained'
+              color='primary'
+              type='submit'>
+              Start game
+            </Button>
+          </form>
+        </Box>
+      )}
 
-			{loader && (
-				<Box display='flex' justifyContent='center'>
-					<CircularProgress/>
-					<p>LOADING...</p>
-				</Box>
-			)}
+      {loader && (
+        <Box display='flex' justifyContent='center'>
+          <CircularProgress/>
+          <p>LOADING...</p>
+        </Box>
+      )}
 
-			{!showStart && (
-				<Box>
-					<Discription>{gameDetails.description}</Discription>
-					<ButtonGroup>
-						{gameDetails.actions.map((action) => (
-							<Button 
-								variant='contained' 
-								color='primary' 
-								key={action.description} 
-								onClick={() => onAction(action)}
-							>
-								{action.type} {action.direction}
-							</Button>
-						))}
-					</ButtonGroup>
-					<Button variant='contained' color='primary' onClick={onHistory}>Go Back</Button>
-				</Box>
-			)}
-		</Container>
-	)
+      {!showStart && (
+        <Box>
+          <Discription>{gameDetails.description}</Discription>
+          <ButtonGroup>
+            {gameDetails.actions.map((action) => (
+              <Button 
+                variant='contained' 
+                color='primary' 
+                key={action.description} 
+                onClick={() => onAction(action)}
+              >
+                {action.type} {action.direction}
+              </Button>
+            ))}
+          </ButtonGroup>
+          <Button variant='contained' color='primary' onClick={onHistory}>Go Back</Button>
+        </Box>
+      )}
+    </Container>
+  )
 }
--- code/src/reducers/games.js
+++ code/src/reducers/games.js
@@ -3,81 +3,81 @@ import { createSlice } from '@reduxjs/toolkit'
 import { ui } from './ui'
 
 export const games = createSlice({
-	name: 'games',
-	initialState: {
-		username: '',
-		actions: [],
-		description: '',
-		coordinates: '',
-		history: [],
-		currentStep:{}
-	},
-	reducers: {
+  name: 'games',
+  initialState: {
+    username: '',
+    actions: [],
+    description: '',
+    coordinates: '',
+    history: [],
+    currentStep:{}
+  },
+  reducers: {
 
-		history: state => {
+    history: state => {
       if (state.history.length > 0) {
-				state.currentStep = state.history[state.history.length - 1]
-				state.description = state.currentStep.description
-				state.actions = state.currentStep.actions
-				state.coordinates = state.currentStep.coordinates
-				state.history = state.history.slice(0, state.history.length -1)
+        state.currentStep = state.history[state.history.length - 1]
+        state.description = state.currentStep.description
+        state.actions = state.currentStep.actions
+        state.coordinates = state.currentStep.coordinates
+        state.history = state.history.slice(0, state.history.length -1)
       }
     },	
 
-		playGame: (store, action) => {
-			store.currentStep = action.payload
-			store.description = action.payload.description
-			store.actions = action.payload.actions
-			store.coordinates = action.payload.coordinates
-		},
+    playGame: (store, action) => {
+      store.currentStep = action.payload
+      store.description = action.payload.description
+      store.actions = action.payload.actions
+      store.coordinates = action.payload.coordinates
+    },
 
-		continueGame: (store, action) => {
-			store.history = [...store.history, store.currentStep]
-			store.currentStep = action.payload
-			store.description = action.payload.description
-			store.actions = action.payload.actions
-			store.coordinates = action.payload.coordinates
-		},
+    continueGame: (store, action) => {
+      store.history = [...store.history, store.currentStep]
+      store.currentStep = action.payload
+      store.description = action.payload.description
+      store.actions = action.payload.actions
+      store.coordinates = action.payload.coordinates
+    },
 
-		setUsername: (store, action) => {
-			store.username = action.payload
-		}
-	}
+    setUsername: (store, action) => {
+      store.username = action.payload
+    }
+  }
 })
 
 export const thunk = (userName) => {
-	return (dispatch) => {
-		dispatch(ui.actions.setLoading(true))
-		fetch('https://wk16-backend.herokuapp.com/start', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ username: userName })
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data)
-				dispatch(games.actions.playGame(data))
-				dispatch(ui.actions.setLoading(false))
-			})
-	}
+  return (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch('https://wk16-backend.herokuapp.com/start', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: userName })
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        dispatch(games.actions.playGame(data))
+        dispatch(ui.actions.setLoading(false))
+      })
+  }
 }
 
 export const actionThunk = (userName, action) => {
-	return (dispatch) => {
-		dispatch(ui.actions.setLoading(true))
-		fetch('https://wk16-backend.herokuapp.com/action', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				username: userName,
-				type: action.type,
-				direction: action.direction,
-			}),
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				dispatch(games.actions.continueGame(data))
-				dispatch(ui.actions.setLoading(false))
-			})
-	}
+  return (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch('https://wk16-backend.herokuapp.com/action', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: userName,
+        type: action.type,
+        direction: action.direction,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(games.actions.continueGame(data))
+        dispatch(ui.actions.setLoading(false))
+      })
+  }
 }
