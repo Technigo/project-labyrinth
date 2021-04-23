d08970062e39e84abac64f053d1ea96a60d054a7
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-04-23 11:54:22 +0200

    test

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactDOM from 'react';
+import Example from './Example'
 
 export const App = () => {
   return (
     <div>
       Find me in src/app.js!
+      <Example />
     </div>
   )
 }
