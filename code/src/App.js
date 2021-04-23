c73500430245b4857ae538e8087e5b25a9e9a5e2
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-04-23 12:00:13 +0200

    fixes after pr

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react';
-import Example from './Example'
+import React from 'react';
+
+import Example from './Example';
 
 export const App = () => {
   return (
