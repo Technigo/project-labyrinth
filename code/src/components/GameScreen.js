39dbc7d5eeef26eb44eb81a8deb2654a7d810427
Rasmus Persson <rrasmuspersson@gmail.com>
2021-04-27 17:28:31 +0200

    removed unnecessary code

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -1,49 +1,46 @@
-import React from 'react';
-import { useDispatch, useSelector,  } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 
-import { secondFetch } from "../reducers/maze"
+import { secondFetch } from "../reducers/maze";
 
 const GameScreen = () => {
+  const description = useSelector(
+    (store) => store.maze.description.description
+  );
 
-    const description = useSelector(store => store.maze.description.description)
+  const actions = useSelector((store) => store.maze.actions);
 
-    const dispatch = useDispatch();
+  const isLoading = useSelector((store) => store.maze.isLoading);
 
-    const actions = useSelector(store => store.maze.actions)
-
-    const isLoading = useSelector(store => store.maze.isLoading )
-
-    if (isLoading === false) {
-        return (
-        <main className='game-screen'>
-        <div className='game-container'>
-            <h1>
-                Welcome!
-            </h1>
-            <p className="white-color"> Description:
-            {description}
-            </p>
-            {actions.map((action) => (
-                <div key={action.description}>
-                <p>{action.description}</p>
-                <button onClick={() => secondFetch(action.direction)}>{action.direction}</button>
-            </div>
-            ))}
-
-        </div>
-    </main>
-    )
-    }
-
+  if (isLoading === false) {
     return (
-        <main className='game-screen'>
-            <div className='game-container'>
-                <h1>
-                    Game is loading... Please wait.
-                </h1>
-                </div>
-        </main>
-    )
-}
+      <main className="game-screen">
+        <div className="game-container">
+          <h1>Welcome!</h1>
+          <p className="white-color">
+            Description:
+            {description}
+          </p>
+          {actions.map((action) => (
+            <div key={action.description}>
+              <p>{action.description}</p>
+              <button onClick={() => secondFetch(action.direction)}>
+                {action.direction}
+              </button>
+            </div>
+          ))}
+        </div>
+      </main>
+    );
+  }
 
-export default GameScreen
+  return (
+    <main className="game-screen">
+      <div className="game-container">
+        <h1>Game is loading... Please wait.</h1>
+      </div>
+    </main>
+  );
+};
+
+export default GameScreen;
