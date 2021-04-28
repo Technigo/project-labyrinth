e8687e3f9032a58e1dc807f89af2a60269a09860
Eleonor Frankenberg <elle@Eleonors-MacBook-Air.local>
2021-04-28 15:55:41 +0200

    made a gamescreen component

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -1,1 +1,11 @@
 import React from 'react'
+
+const GameScreen = () => {
+
+  return (
+    <div> This is a description</div>
+
+  )
+}
+
+export default GameScreen
--- code/src/components/Games.js
+++ code/src/components/Games.js
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+ 
 import StartScreen from './StartScreen'
+import GameScreen from './GameScreen'
 
 const Games = () => {
+  const description = useSelector(store => store.games.description)
+  console.log(description)
+
   return (
-    <StartScreen />
+    <div>
+      {description
+        ? <GameScreen />
+        : <StartScreen />
+      }
+    </div>
+  
 
   )
 }
