b3d119e0cc102b70d5d483f2e13287c1e6d56616
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-04-29 13:50:07 +0200

    started with styling of startscreen

--- code/src/components/StartScreen.js
+++ code/src/components/StartScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import styled from 'styled-components/macro'
 
 import { games, createNewPlayer } from '../reducers/games'
 
@@ -19,16 +20,55 @@ const StartScreen = () => {
     }
 
     return (
-        <form onSubmit={onFormSubmit}>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={event => setInputValue(event.target.value)}
-                placeholder="Type your name..."
-            />
-            <button onClick={onFormSubmit}>Start</button>
-        </form>
+        <StartScreenBox>
+            <StartScreenTitle>Insert Name and get ready to play</StartScreenTitle>
+            <StartScreenForm onSubmit={onFormSubmit}>
+                <StartScreenInput
+                    type="text"
+                    value={inputValue}
+                    onChange={event => setInputValue(event.target.value)}
+                    placeholder="Type your name..."
+                />
+                <StartScreenButton onClick={onFormSubmit}>Start</StartScreenButton>
+            </StartScreenForm>
+        </StartScreenBox>
     )
 }
 
+const StartScreenBox = styled.div `
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    background-color: black;
+    background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);
+    width: 400px;
+    height: 300px;
+`
+
+const StartScreenTitle = styled.h1`
+    font-size: 18px;
+    color: white;
+    font-family: Inconsolata, monospace;
+    text-shadow: 0 0 5px #C8C8C8;
+`
+
+const StartScreenForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const StartScreenInput = styled.input`
+    margin: 0 0 20px 0;
+    width: 200px;
+    background-color: black;
+    color: grey;
+    border-color: grey;
+`
+
+const StartScreenButton = styled.button`
+    width: 100px;
+`
 export default StartScreen
