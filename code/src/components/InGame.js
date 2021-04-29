c3292cadc43d65348ea60d1c7933c5a1c0a5c17c
newmarkdev <newmarkdev@gmail.com>
2021-04-29 12:09:19 +0200

    fix:display json data for description

--- code/src/components/InGame.js
+++ code/src/components/InGame.js
@@ -1,16 +1,30 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-import game from 'reducers/game'
+import game, { nextStep } from 'reducers/game'
 
 const InGame = () => {
   const direction = useSelector((store) => store.game.direction)
+  const actions = useSelector((store) => store.game.direction.actions)
+  const username = useSelector((store) => store.game.username)
+  const dispatch = useDispatch() 
 
+  console.log('action:', actions)
+
+  console.log(direction)
 
 
   return (
     <div>
-        <p>test: {direction && direction.description}</p>
+        <p>{direction.description}</p>
+        {
+          actions.map((action) => (
+            <div key={action.description}>
+              <p>{action.description}</p>
+              <button onClick={() => dispatch(nextStep(username, action.direction))}>{action.direction}</button>
+            </div>
+          ))
+        }
   </div>
   )
 }
