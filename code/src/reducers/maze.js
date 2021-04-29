6b422c50ff56c2660ee3d769ad0d1640ca9b8874
Rasmus Persson <rrasmuspersson@gmail.com>
2021-04-29 08:21:13 +0200

    dispatched the coordinates to the history array

--- code/src/reducers/maze.js
+++ code/src/reducers/maze.js
@@ -1,86 +1,89 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const maze = createSlice({
-    name: 'maze',
-    initialState: {
-        username: '',
-        description: '',
-        moves: null,
-        history: [],
-        isLoading: false,
-        error: null
-    } ,
-    reducers: {
-        setUsername: (store, action) => {
-            store.username = action.payload;
-        }, 
-        setMoves: (store, action) => {
-            store.moves = action.payload
-        },
-        setDescription: (store, action) => {
-            store.description = action.payload;
-        },
-        setLoading: (store, action) => {
-            store.isLoading = action.payload
-        },
-        setHistory: (store, action) => {
-            store.history = [...store.history, action.payload]
-        },
-        setError: (store, action) => {
-            store.error = action.payload
-        }
-    }
-})
+  name: "maze",
+  initialState: {
+    username: "",
+    description: "",
+    moves: null,
+    history: [],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    setUsername: (store, action) => {
+      store.username = action.payload;
+    },
+    setMoves: (store, action) => {
+      store.moves = action.payload;
+    },
+    setDescription: (store, action) => {
+      store.description = action.payload;
+    },
+    setLoading: (store, action) => {
+      store.isLoading = action.payload;
+    },
+    setHistory: (store, action) => {
+      store.history = [...store.history, action.payload];
+    },
+    setError: (store, action) => {
+      store.error = action.payload;
+    },
+  },
+});
 
-export default maze
+export default maze;
 
 export const firstFetch = () => {
-    return (dispatch, getState) => {
-      fetch("https://wk16-backend.herokuapp.com/start", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username: getState().maze.username }),
+  return (dispatch, getState) => {
+    fetch("https://wk16-backend.herokuapp.com/start", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: getState().maze.username }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          dispatch(maze.actions.setError(null));
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
       })
-        .then((response) => {
-            if (response.ok) {
-                dispatch(maze.actions.setError(null))
-                return response.json()
-            } else {
-                throw new Error(response.statusText)}
-            })
-        .then((data) => {
-            dispatch(maze.actions.setDescription(data.description))
-            dispatch(maze.actions.setMoves(data.actions))
-        })
-        .catch(error => dispatch(maze.actions.setError(error.message)))
-        .finally(() => dispatch(maze.actions.setLoading(false)))
-    }
-  }
-
+      .then((data) => {
+        dispatch(maze.actions.setDescription(data.description));
+        dispatch(maze.actions.setMoves(data.actions));
+        dispatch(maze.actions.setHistory(data.coordinates));
+      })
+      .catch((error) => dispatch(maze.actions.setError(error.message)))
+      .finally(() => dispatch(maze.actions.setLoading(false)));
+  };
+};
 
 export const secondFetch = (direction) => {
-    return (dispatch, getState) => {
-        fetch("https://wk16-backend.herokuapp.com/action", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                username: getState().maze.username,
-                type: "move",
-                direction: direction
-            }),
-        })
-        .then((response) => {
-            if (response.ok) {
-                dispatch(maze.actions.setError(null))
-                return response.json()
-            } else {
-                throw new Error(response.statusText)}
-            })
-        .then((data) => {
-            dispatch(maze.actions.setDescription(data.description))
-            dispatch(maze.actions.setMoves(data.actions))
-        })
-        .catch(error => dispatch(maze.actions.setError(error.message)))
-        .finally(() => dispatch(maze.actions.setLoading(false)))
-    }
-}
+  return (dispatch, getState) => {
+    fetch("https://wk16-backend.herokuapp.com/action", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: getState().maze.username,
+        type: "move",
+        direction: direction,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          dispatch(maze.actions.setError(null));
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        dispatch(maze.actions.setDescription(data.description));
+        dispatch(maze.actions.setMoves(data.actions));
+        dispatch(maze.actions.setHistory(data.coordinates));
+      })
+      .catch((error) => dispatch(maze.actions.setError(error.message)))
+      .finally(() => dispatch(maze.actions.setLoading(false)));
+  };
+};
