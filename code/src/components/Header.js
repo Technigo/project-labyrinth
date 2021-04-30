44540ba7f905bb12bbc5bc7f4a3125e5f46ef39a
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-04-30 09:56:55 +0200

    added some styling

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react' 
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const Header = () => {
   
@@ -9,16 +9,22 @@ const Header = () => {
 
   return (
     <HeaderContainer>
-        <button className="nes-btn" onClick={onRestartButton}>
-            Restart
+        <i className="nes-icon is-medium star"></i>
+        <button className="nes-btn is-warning" onClick={onRestartButton}>
+           Restart
         </button>
+        <i className="nes-icon is-medium star"></i>
     </HeaderContainer>
   )
 }
 
 const HeaderContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   background-color: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between
 `;
 
 export default Header
