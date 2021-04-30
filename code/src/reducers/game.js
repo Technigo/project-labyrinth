50518071a1d2bbc0fe0a5dbac03e7a3b7ccd05a6
Lars Söderman <lars.soderman@gmail.com>
2021-04-30 14:58:56 +0200

    show end screen

--- code/src/reducers/game.js
+++ code/src/reducers/game.js
@@ -1,73 +1,77 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit'
 
-import { startUrl, actionUrl } from "../reusables/urls";
+import { startUrl, actionUrl } from '../reusables/urls'
 
 const game = createSlice({
-  name: "game",
+  name: 'game',
   initialState: {
     userName: null,
     gameData: null,
     gameStart: false,
     history: [],
+    gameOver: false,
   },
   reducers: {
     setUserName: (store, action) => {
-      store.userName = action.payload;
+      store.userName = action.payload
     },
     setGameData: (store, action) => {
       if (store.gameData) {
-        store.history = [...store.history, store.gameData];
+        store.history = [...store.history, store.gameData]
       }
-      store.gameData = action.payload;
+      store.gameData = action.payload
     },
     setGameStart: (store, action) => {
-      store.gameStart = action.payload;
+      store.gameStart = action.payload
     },
     setPreviousStep: (store, action) => {
       if (store.history.length) {
-        store.gameData = store.history[store.history.length - 1];
-        store.history = store.history.slice(0, store.history.length - 1);
+        store.gameData = store.history[store.history.length - 1]
+        store.history = store.history.slice(0, store.history.length - 1)
       }
     },
     setResetGame: (store, action) => {
-      store.userName = null;
-      store.gameData = null;
-      store.gameStart = false;
-      store.history = [];
+      store.userName = null
+      store.gameData = null
+      store.gameStart = false
+      store.history = []
+    },
+    setGameOver: (store, action) => {
+      store.gameOver = action.payload
     },
   },
-});
+})
 
 export const generateGame = (userName) => {
   return (dispatch) => {
     fetch(startUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ username: userName }),
     })
       .then((res) => res.json())
-      .then((gameData) => dispatch(game.actions.setGameData(gameData)));
-  };
-};
+      .then((gameData) => dispatch(game.actions.setGameData(gameData)))
+  }
+}
 
 export const continueGame = (direction) => {
   return (dispatch, getState) => {
     fetch(actionUrl, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         username: getState().game.userName,
-        type: "move",
+        type: 'move',
         direction: direction,
       }),
     })
       .then((res) => res.json())
-      .then((gameData) => dispatch(game.actions.setGameData(gameData)));
-  };
-};
+      .then((gameData) => dispatch(game.actions.setGameData(gameData)))
+  }
+}
 
-export default game;
+export default game
