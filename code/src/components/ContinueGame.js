26c65410b5816602f761322de3a4a0f26320811d
Sara Carlstein <carlstein.sara@gmail.com>
2021-05-07 16:47:21 +0200

    styling

--- code/src/components/ContinueGame.js
+++ code/src/components/ContinueGame.js
@@ -1,55 +1,77 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import "nes.css/css/nes.min.css"
-import styled from 'styled-components'
-import { EndPage } from './EndPage'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "nes.css/css/nes.min.css";
+import styled from "styled-components";
+import { EndPage } from "./EndPage";
 
-import { labyrinth, generateGameProgress } from '../reducers/game'
+import { labyrinth, generateGameProgress } from "../reducers/game";
 
+const Border = styled.div`
+  border: 3px solid whitesmoke;
+  padding: 15px;
+  margin-bottom: 15px;
+`;
 
 const Container = styled.div`
-    width: 900px;
-    height: 450px;
-    margin-top: 50px;
-    
-    @media (max-width: 768px) {
-      height: 900px;
-      margin-top: 20px;
-     };
-`
+  width: 900px;
+  height: 700px;
+  margin-top: 50px;
+
+  @media (max-width: 768px) {
+    height: 900px;
+    margin-top: 20px;
+  } ;
+`;
 
 const ContinueGame = () => {
-    const dispatch = useDispatch()
-    const currentState = useSelector((state) => state);
-    const playerName =  currentState.labyrinth.username;
-    const actions = currentState.labyrinth.game.actions;
+  const dispatch = useDispatch();
+  const currentState = useSelector((state) => state);
+  const playerName = currentState.labyrinth.username;
+  const actions = currentState.labyrinth.game.actions;
 
+  const onGameContinue = (direction) => {
+    dispatch(generateGameProgress(playerName, direction));
+  };
 
-    const onGameContinue = (direction) => { 
-        dispatch(generateGameProgress(playerName,direction));
-      };
-    
-    // Function that gets invoked by clicking the back button
-    const onGoBack = () => { 
-      dispatch(labyrinth.actions.historyGoBack());
-    };
+  // Function that gets invoked by clicking the back button
+  const onGoBack = () => {
+    dispatch(labyrinth.actions.historyGoBack());
+  };
 
-    console.log("Current state of the game: ", currentState)
+  console.log("Current state of the game: ", currentState);
 
-    if (actions.length === 0) { 
-        return <EndPage/>
-      };
+  if (actions.length === 0) {
+    return <EndPage />;
+  }
 
-    return ( 
-        <Container className="nes-container is-dark with-title">
-          <h3>{currentState.labyrinth.game.description}</h3>
-          {actions.map((action)=> 
-              <button type="button" class="nes-btn is-primary" key= {action.description} onClick={() => onGameContinue(action.direction)}> Go {action.direction}</button>
-            )}
-            <p>*HINT* {currentState.labyrinth.game.actions[0]?.description}</p>
-          <button type="button" class="nes-btn is-error" onClick={onGoBack}>BACK</button>
-        </Container>
-    )
-}
+  return (
+    <Container className="nes-container is-dark with-title">
+      <h3>{currentState.labyrinth.game.description}</h3>
+      <div>
+        {actions.map((action) => (
+          <div>
+            <Border className="button-text-container">
+              <p>{action.description}</p>
+              <button
+                type="button"
+                class="nes-btn is-primary"
+                key={action.description}
+                onClick={() => onGameContinue(action.direction)}
+              >
+                Go {action.direction}
+              </button>
+            </Border>
+          </div>
+        ))}
+      </div>
 
-export default ContinueGame
+      <div>
+        <button type="button" class="nes-btn is-error" onClick={onGoBack}>
+          BACK
+        </button>
+      </div>
+    </Container>
+  );
+};
+
+export default ContinueGame;
--- code/src/components/InitateGame.js
+++ code/src/components/InitateGame.js
@@ -1,59 +1,94 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import "nes.css/css/nes.min.css"
-import styled from 'styled-components'
-import { labyrinth, generateStartInstructions } from '../reducers/game'
-import ContinueGame from "./ContinueGame"
-import StartGame from "./StartGame"
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "nes.css/css/nes.min.css";
+import styled from "styled-components";
+import { labyrinth, generateStartInstructions } from "../reducers/game";
+import ContinueGame from "./ContinueGame";
+import StartGame from "./StartGame";
+
+const Welcome = styled.div`
+  margin-bottom: 20px;
+  margin-top: -300px;
+  margin-left: 80px;
+`;
+
+const Message = styled.div`
+  margin-left: 90px;
+  color: black;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+`;
 
 const Container = styled.div`
-    width: 900px;
-    height: 300px;
-`
+  width: 900px;
+  height: 300px;
+`;
 
 const InitiateGame = () => {
-    //controlled input we need states: 
-    const [inputValue, setInputValue] = useState("")
-    const dispatch = useDispatch()
-    const gameState = useSelector((state) => state.labyrinth.game);
-    const playerName = useSelector((state) => state.labyrinth.username)
- 
-    const isStartingGame = () => { 
-        return playerName && !gameState.coordinates;
-      };
+  //controlled input we need states:
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch();
+  const gameState = useSelector((state) => state.labyrinth.game);
+  const playerName = useSelector((state) => state.labyrinth.username);
 
-      const isContinuingGame = () => { 
-        return playerName && gameState.coordinates;
-      };
-     
-    const setGame = () => {
-        //dispatching an action that takes the inputValue to change username
-        dispatch(labyrinth.actions.setPlayerName(inputValue)) 
-        //dispathing thunk function
-        dispatch(generateStartInstructions(playerName))
-    } 
+  const isStartingGame = () => {
+    return playerName && !gameState.coordinates;
+  };
 
-    if (isStartingGame()) {
-        return <StartGame/>
-      } else if (isContinuingGame()) { 
-        return <ContinueGame />
-      };
+  const isContinuingGame = () => {
+    return playerName && gameState.coordinates;
+  };
 
-    return (
-        <Container className="nes-container is-dark with-title">
-            <p className="title">Welcome to the labyrinth game</p>
-            <p>Type in your unique username<i class="snes-jp-logo"></i></p>
-            <input
-                id="dark_field"
-                type="text"
-                value={inputValue}
-                onChange={event => setInputValue(event.target.value)}
-                className="nes-input is-dark" 
-                placeholder="e.g. Saraxx90"
-            />
-            <button type="button" className="nes-btn is-success" onClick={setGame}>Create Player</button>
-        </Container>
-    )
-}
+  const setGame = () => {
+    //dispatching an action that takes the inputValue to change username
+    dispatch(labyrinth.actions.setPlayerName(inputValue));
+    //dispathing thunk function
+    dispatch(generateStartInstructions(playerName));
+  };
 
-export default InitiateGame
+  if (isStartingGame()) {
+    return <StartGame />;
+  } else if (isContinuingGame()) {
+    return <ContinueGame />;
+  }
+
+  return (
+    <Wrapper>
+      <Welcome>
+        <section>
+          <Message>
+            <div className="ballon" class="nes-balloon from-left">
+              Welcome to the labyrinth game
+            </div>
+          </Message>
+          <i class="nes-ash"></i>
+        </section>
+      </Welcome>
+
+      <Container className="nes-container is-dark with-title">
+        <p className="title">Create player</p>
+        <p>
+          Type in your unique username<i class="snes-jp-logo"></i>
+        </p>
+        <input
+          id="dark_field"
+          type="text"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          className="nes-input is-dark"
+          placeholder="e.g. Hippoxx34"
+        />
+        <button type="button" className="nes-btn is-success" onClick={setGame}>
+          Create Player
+        </button>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default InitiateGame;
