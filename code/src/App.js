f7402ce881112adb2bb5796064842657af4340d2
elsisco <elsa.carlstrom@gmail.com>
2021-11-22 14:55:27 +0100

    first push

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+
+// import Start from './pages/Start'
+// import QuestionPage from './pages/QuestionPage'
+// import Summary from './pages/Summary'
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// import { quiz } from 'reducers/quiz'
+
+const reducer = combineReducers({
+  // quiz: quiz.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" exact element={<Start />} />
+          <Route path="/questions" element={<QuestionPage />} />
+          <Route path="/summary" element={<Summary />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
---
+++ code/src/reducers/game.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { ui } from './ui'
+
+export const game = createSlice({
+  name: 'game',
+  initialState: {
+    username: 'player1'
+  },
+  reducers: {}
+})
+
+export const gameStart = () => {
+  return (dispatch) => {
+    dispatch(ui.actions.setLoading(true))
+    fetch('https://wk16-backend.herokuapp.com/start', {
+      method: 'POST'
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(game.actions.startGame(json))
+        dispatch(ui.actions.setLoading(false))
+      })
+  }
+}
+
+// fetch('', {
+//     method: 'POST'
+//   })
+//     .then((res) => res.json())
+//     .then(() => {
+//       fetchThoughts()
+//       setYourLikes((value) => value + 1)
+//     }, [])
