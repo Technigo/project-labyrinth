e2e57ff9478a395e0fa3f002eef780919db06128
Idautterstrom <ida.utterstrom@hotmail.com>
2021-11-23 12:28:20 +0100

    styling added

--- code/src/components/StartPage.js
+++ code/src/components/StartPage.js
@@ -1,25 +1,50 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { startGame } from 'reducers/game'
-// import styled from 'styled-components'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { startGame } from "reducers/game";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 300px;
+`;
+
+const Header = styled.h1`
+  font-size: 20px;
+`;
+
+const SmallHeader = styled.h2`
+  font-size: 15px;
+`;
+
+const Input = styled.input`
+  height: 20px;
+  width: 250px;
+`;
 
 export const StartPage = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   // const username = useSelector(store => store.game.username)
 
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
 
   const onStartGame = () => {
-    dispatch(startGame(input))
-  }
+    dispatch(startGame(input));
+  };
 
   return (
     <div>
-      <h1>What's your name?</h1>
-      <input type='text' value={input} onChange={e => setInput(e.target.value)} />
-      <button disabled={input === ''} onClick={onStartGame}>
+      <Header>Let's play Labyrinth!</Header>
+      <SmallHeader>What's your name?</SmallHeader>
+      <Input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button disabled={input === ""} onClick={onStartGame}>
         Start game!
       </button>
     </div>
-  )
-}
+  );
+};
