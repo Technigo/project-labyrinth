77fd4827dd7319bd2ada5a9bcceef4ef5c590890
Naushin Malik <nosheen.malik@gmail.com>
2021-11-23 12:27:48 +0100

    fixed the issue in fetching data

--- code/src/components/Labyrinth.js
+++ code/src/components/Labyrinth.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchLabyrinth } from "../reducers/labyrinth";
-import { labyrinth } from "../reducers/labyrinth";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchLabyrinth } from '../reducers/labyrinth';
+import { labyrinth } from '../reducers/labyrinth';
 
 const Labyrinth = () => {
-    const labyrinth = useSelector((store) => store.labyrinth.destination);
-    const dispatch = useDispatch();
+  const labyrinth = useSelector((store) => store.labyrinth.destination);
+  const dispatch = useDispatch();
 
-    return (
-        <button onClick={fetchLabyrinth}>
-            Start Labyrinth-Game!
-        </button>
-
-    )
-}
-
+  return (
+    <button
+      onClick={() => {
+        dispatch(fetchLabyrinth());
+      }}
+    >
+      Start Labyrinth-Game!
+    </button>
+  );
+};
 
 export default Labyrinth;
-
-
-
--- code/src/reducers/labyrinth.js
+++ code/src/reducers/labyrinth.js
@@ -1,31 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 export const labyrinth = createSlice({
-    name: 'labyrinth',
-    initialState: {
-        destination: [{}]
+  name: 'labyrinth',
+  initialState: {
+    destination: [{}],
+  },
+  reducers: {
+    setLabyrinthPath: (state, action) => {
+      state.destination = action.payload;
     },
-    reducers: {
-        setLabyrinthPath: (state, action) => {
-            state.destination = action.payload;
-        }
-    }
+  },
 });
 
 export const fetchLabyrinth = () => {
-    return (dispatch) => {
-       // dispatch loading component with actions.set...(true)
+  console.log('i am here');
+  return (dispatch) => {
+    // dispatch loading component with actions.set...(true)
     fetch('https://wk16-backend.herokuapp.com/start', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(
-            {username: 'Player'}), 
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            console.log (json)
-            dispatch(labyrinth.actions.setLabyrinthPath(json)); 
-            // add code for set loading false
-        })
-    }
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: 'Player' }),
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        dispatch(labyrinth.actions.setLabyrinthPath(json));
+        // add code for set loading false
+      });
+  };
 };
