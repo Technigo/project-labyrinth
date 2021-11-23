4608c72644a0fb3d9ba60d76558c3d3d69ba4f71
JessicaNordahl <jessica.nordahl@hotmail.com>
2021-11-23 15:30:18 +0100

    Resolved problem with displaying fetch from api. Added a condition rendering the data

--- code/src/components/Labyrinth.js
+++ code/src/components/Labyrinth.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchLabyrinth } from '../reducers/labyrinth';
+import { fetchLabyrinth, fetchActions } from '../reducers/labyrinth';
 import { labyrinth } from '../reducers/labyrinth';
 
 const Labyrinth = () => {
-  const labyrinth = useSelector((store) => store.labyrinth.destination);
-  const dispatch = useDispatch();
-
-  return (
+    const labyrinth = useSelector((store) => store.labyrinth.destination);
+    const dispatch = useDispatch();
+    console.log ('actions', labyrinth.actions)
+return (
+    <>
     <button
-      onClick={() => {
+        onClick={() => {
         dispatch(fetchLabyrinth());
-      }}
-    >
-      Start Labyrinth-Game!
+        }}
+        >
+        Start Labyrinth-Game!
     </button>
-  );
+    <p>
+        {labyrinth.description}
+        {console.log ('actions2', labyrinth.actions)}
+    </p>
+    <div>
+        
+        {labyrinth.actions !== undefined &&
+        labyrinth.actions.map((item) => (
+        <>
+            <p key={item.description}>
+                {item.description} 
+            </p>
+            <button onCLick={() => {
+                dispatch(fetchActions(item.direction))
+            }}>{item.direction}</button>
+        </>    
+        )) }
+
+        
+    </div>
+    
+    </>
+);
 };
 
 export default Labyrinth;
--- code/src/reducers/labyrinth.js
+++ code/src/reducers/labyrinth.js
@@ -1,31 +1,51 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const labyrinth = createSlice({
-  name: 'labyrinth',
-  initialState: {
-    destination: [{}],
-  },
-  reducers: {
-    setLabyrinthPath: (state, action) => {
-      state.destination = action.payload;
+    name: 'labyrinth',
+    initialState: {
+    destination: [],
     },
-  },
+    reducers: {
+    setLabyrinthPath: (state, action) => {
+        state.destination = action.payload;
+    },
+    },
 });
 
 export const fetchLabyrinth = () => {
-  console.log('i am here');
-  return (dispatch) => {
+    console.log('i am here');
+    return (dispatch) => {
     // dispatch loading component with actions.set...(true)
     fetch('https://wk16-backend.herokuapp.com/start', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: 'Player' }),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: 'gulrukh' }),
     })
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json);
+        .then((res) => res.json())
+        .then((json) => {
         dispatch(labyrinth.actions.setLabyrinthPath(json));
         // add code for set loading false
-      });
-  };
+        });
+    };
 };
+
+export const fetchActions = ({direction}) => {
+    console.log(direction)
+    return(dispatch) => {
+        fetch('https://wk16-backend.herokuapp.com/action', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(
+                { username: 'gulrukh',
+                type: 'move',
+                direction: direction,
+        }),
+    })
+        .then((res) => res.json())
+        .then((json) => {
+        dispatch(labyrinth.actions.setLabyrinthPath(json));
+        // add code for set loading false
+        });
+    };
+};
+ 
