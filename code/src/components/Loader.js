19533d623b54d5feb836dc8fce9b292d5cb54563
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2021-11-23 20:12:49 +0100

    update layout of loading animation

--- code/src/components/Loader.js
+++ code/src/components/Loader.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(246, 246, 246, 0.5);
+`;
+
+const spin = keyframes`
+ 0% { transform: rotate(0deg) }
+ 100% { transform: rotate(360deg) }
+`;
+
+const LoadingSpinner = styled.div`
+  background: linear-gradient(
+    -45deg,
+    rgba(255, 255, 255, 0) 49%,
+    rgb(255, 21, 0) 51%
+  );
+  border-radius: 50%;
+  width: 350px;
+  height: 350px;
+  animation: ${spin} 2s linear infinite;
+`;
 
 const Loader = () => {
-  return <div>This is a loader</div>;
+  return (
+    <LoaderContainer>
+      <LoadingSpinner />
+    </LoaderContainer>
+  );
 };
 
 export default Loader;
