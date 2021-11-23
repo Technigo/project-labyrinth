6d58b9808588aa9b31325259a110c725ce5abaaa
JessicaNordahl <jessica.nordahl@hotmail.com>
2021-11-23 11:30:12 +0100

    First commit. Installed npm, toolkit and react-redux.  Created reducer and component

---
+++ code/src/components/Labyrinth.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchLabyrinth } from "../reducers/labyrinth";
+import { labyrinth } from "../reducers/labyrinth";
+
+const Labyrinth = () => {
+    const labyrinth = useSelector((store) => store.labyrinth.destination);
+    const dispatch = useDispatch();
+
+    return (
+        <button onClick={fetchLabyrinth}>
+            Start Labyrinth-Game!
+        </button>
+
+    )
+}
+
+
+export default Labyrinth;
+
+
+
