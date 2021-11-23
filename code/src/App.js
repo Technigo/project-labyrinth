aa0798052a31c1dd895d295cac7746c0da21e954
Birgit Nehrwein <nehrwein@posteo.de>
2021-11-23 14:35:22 +0100

    added urls.js and made the fetches in game.js. fetch1 is tested and works. fetch2 is untested

--- code/src/App.js
+++ code/src/App.js
@@ -4,7 +4,7 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit'
 
 import { ui } from './reducers/ui'
 import { game } from './reducers/game'
-import LandingPage from 'pages/LandingPage'
+import StartGame from 'pages/StartGame'
 
 const reducer = combineReducers({
   ui: ui.reducer,
@@ -13,10 +13,11 @@ const reducer = combineReducers({
 
 const store = configureStore({ reducer })
 
-//we still need to import the loading indicator <LoadingSpinner />
+//we still need to import the loading indicator <LoadingIndicator />
 export const App = () => {
   return (
     <Provider store={store}>
+      <StartGame />
     </Provider>
   )
 }
---
+++ code/src/utils/urls.js
@@ -0,0 +1,2 @@
+export const START_API_URL = 'https://wk16-backend.herokuapp.com/start'
+export const NEXT_PLACE_API_URL = 'https://wk16-backend.herokuapp.com/action'
