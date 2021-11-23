be9deb17299387b7a8d819fc9cf7afc60c3a5f8a
camekman <ekman.camilla@gmail.com>
2021-11-23 16:36:25 +0100

    second draft

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -6,26 +6,27 @@ import { StartGame } from "./StartGame";
 
 export const Game = () => {
   const gameList = useSelector((store) => store.game.gameList);
+  //   const username = useSelector((store) => store.game.username);
   const dispatch = useDispatch();
 
   return (
     <>
-      {gameList.coordinates < 0 && <StartGame />}
-      {gameList.coordinates === 0.0 && (
-        <>
-          <div>
-            <p>Coordinates: {gameList.coordinates}</p>
-            <h1>{gameList.description}</h1>
-          </div>
-          <div>
-            {/* {gameList.actions.map((item) => (
-          <div key={item.description}>
-            <button>Move {item.direction}</button>
-          </div>
-        ))} */}
-          </div>
-        </>
-      )}
+      {gameList === null && <StartGame />}
+
+      <div>
+        <p>Coordinates: {gameList.coordinates}</p>
+        <h1>{gameList.description}</h1>
+      </div>
+      <div>
+        {/* {gameList.actions.map((item) => (
+        <div key={item.description}>
+          <button>Move {item.direction}</button>
+        </div>
+      ))} */}
+      </div>
     </>
   );
 };
+
+// {gameList.coordinates < 0 && <StartGame />}
+// {gameList.coordinates === 0.0 &&
