e89e910750d545c308a78525aa31eca6d378e204
HedvigM <hedvig@mejstedt.se>
2021-11-23 17:29:48 +0100

    First fetch

--- code/src/App.js
+++ code/src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
+import { labyrinth } from './reducers/description';
+
+import { StartPage } from 'components/StartPage';
+import { Description } from 'components/Description';
+
 const reducer = combineReducers({
-  /*  xxx: xxx.reducer */
+  labyrinth: labyrinth.reducer
+  /*   loader: loader.reducer, */
 });
 
 const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <Provider>
-      <div>Find me in src/app.js!</div>
+    <Provider store={store}>
+      <StartPage />
+      {/* <Description /> */}
     </Provider>
   );
 };
---
+++ code/src/components/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Description = () => {
+  /*  const description = useSelector((state) => state.XXXXXX) */
+  return (
+    <div>
+      <h1>Description</h1>
+      {/*  {description.map((d) => (
+         <>
+          <p>{d.description}</p>
+          <p>{d.coordinates}</p>
+        </>
+      ))} */}
+    </div>
+  );
+};
