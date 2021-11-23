aab50bd5f69f0a4f74994eec74e111ad662cda53
AschwinSiaila <aschwin76@hotmail.com>
2021-11-23 14:39:54 +0100

    routes

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import React from "react";
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { ui } from './reducers/ui'
-import { game } from './reducers/game'
-import LandingPage from 'pages/LandingPage'
+import { ui } from "./reducers/ui";
+import { game } from "./reducers/game";
+import LandingPage from "pages/LandingPage";
 
 const reducer = combineReducers({
   ui: ui.reducer,
-  game: game.reducer
-})
+  game: game.reducer,
+});
 
-const store = configureStore({ reducer })
+const store = configureStore({ reducer });
 
 //we still need to import the loading indicator <LoadingSpinner />
 export const App = () => {
   return (
     <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<StartGame />} />
+          <Route path="/" element={<Game />} />
+          <Route path="/" element={<End />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  )
-}
+  );
+};
