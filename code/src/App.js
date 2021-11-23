f293a61c05b45200ecb328782822029f76c0620c
HedvigM <hedvig@mejstedt.se>
2021-11-23 17:36:56 +0100

    Revert "First fetch"
    This reverts commit e89e910750d545c308a78525aa31eca6d378e204.

--- code/src/App.js
+++ code/src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
-import { labyrinth } from './reducers/description';
-
-import { StartPage } from 'components/StartPage';
-import { Description } from 'components/Description';
-
 const reducer = combineReducers({
-  labyrinth: labyrinth.reducer
-  /*   loader: loader.reducer, */
+  /*  xxx: xxx.reducer */
 });
 
 const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <StartPage />
-      {/* <Description /> */}
+    <Provider>
+      <div>Find me in src/app.js!</div>
     </Provider>
   );
 };
