4550f8b4940aa843d0c2c45d44cb806194aa9ecd
loulunds <lousanne.lundstrom@gmail.com>
2021-11-24 16:38:50 +0100

    hover show text

--- code/src/components/GameBoard.js
+++ code/src/components/GameBoard.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { MainGame } from './MainGame';
 import { StartPage } from './StartPage';
 import { useSelector } from 'react-redux';
-import { game } from 'reducers/game';
-import EndPage from './EndPage';
 
 const GameBoard = () => {
   const game = useSelector((state) => state.game);
