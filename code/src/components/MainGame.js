d4c4d6920f4f5a7dad79104cf6b9d83145dcaa2b
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-11-24 14:14:43 +0100

    typewriter added

--- code/src/components/MainGame.js
+++ code/src/components/MainGame.js
@@ -1,32 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import styled, { keyframes } from 'styled-components';
-import { useWindowSize } from '@react-hook/window-size';
+import styled, { keyframes } from "styled-components";
+import { useWindowSize } from "@react-hook/window-size";
 
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import GameStage from './GameStage';
-import ceiling from '../assets/ceiling.png';
-import cave from '../assets/cave.png';
-import last from '../assets/last.png';
-import color from '../assets/color.png';
-import paper from '../assets/paper.png';
-import steampunk from '../assets/steampunk.png';
-import colorfulcave from '../assets/colorfulcave.png';
+import GameStage from "./GameStage";
+import ceiling from "../assets/ceiling.png";
+import cave from "../assets/cave.png";
+import last from "../assets/last.png";
+import color from "../assets/color.png";
+import paper from "../assets/paper.png";
+import steampunk from "../assets/steampunk.png";
+import colorfulcave from "../assets/colorfulcave.png";
 
-const typing = keyframes`
-	from {width: 0};
-    to {width:100%};
-`;
+import TypeWriterEffect from "react-typewriter-effect";
 
-const blinkCaret = keyframes`
-	from,
-    to {border-color: transparent;
-  }
-  50% {
-    border-color: orange;
-  }
-`;
+// const typing = keyframes`
+// 	from {width: 0};
+//     to {width:100%};
+// `;
+
+// const blinkCaret = keyframes`
+// 	from,
+//     to {border-color: transparent;
+//   }
+//   50% {
+//     border-color: orange;
+//   }
+// `;
 
 const GameBox = styled.div`
   height: ${(props) => props.hgt}px;
@@ -43,43 +45,32 @@ const GameBox = styled.div`
   align-items: center;
   color: white;
 
-  .typewriter {
+  /* .typewriter {
     word-break: break-word;
     max-width: 65%;
     display: inline-block;
-  }
-  .typewriter h1 {
-    overflow: hidden; /* Ensures the content is not revealed until the animation */
-    border-right: 0.15em solid orange; /* The typwriter cursor */
+  } */
+  /* .typewriter h1 {
+    overflow: hidden;
+    border-right: 0.15em solid orange; 
     white-space: normal;
-    margin: 0 auto; /* Gives that scrolling effect as the typing happens */
-    letter-spacing: 0.15em; /* Adjust as needed */
+    margin: 0 auto;
+    letter-spacing: 0.15em;
     animation: typing 3.5s steps(40, end), blinkCaret 0.75s step-end infinite;
     font-size: 1em;
-    font-family: 'Libre Baskerville', serif;
+    font-family: "Libre Baskerville", serif;
     margin-top: 20px;
-  }
+  } */
 
   /* The typing effect */
-  @keyframes typing {
+  /* @keyframes typing {
     from {
       width: 0;
     }
     to {
       width: 100%;
     }
-  }
-
-  /* The typewriter cursor effect */
-  @keyframes blink-caret {
-    from,
-    to {
-      border-color: transparent;
-    }
-    50% {
-      border-color: orange;
-    }
-  }
+  }  */
 `;
 
 const DescriptionBox = styled.div`
@@ -96,22 +87,22 @@ const DescriptionBox = styled.div`
 export const MainGame = () => {
   const bgSwitcher = (coordinates) => {
     switch (coordinates) {
-      case '0,0':
+      case "0,0":
         return cave;
-      case '1,0':
+      case "1,0":
         return ceiling;
-      case '1,1':
+      case "1,1":
         return colorfulcave;
-      case '0,1':
+      case "0,1":
         return steampunk;
-      case '0,2':
+      case "0,2":
         return color;
-      case '0,3':
+      case "0,3":
         return paper;
-      case '1,3':
+      case "1,3":
         return last;
       default:
-        return 'error';
+        return "error";
     }
   };
 
@@ -126,9 +117,14 @@ export const MainGame = () => {
       wid={width}
     >
       <DescriptionBox>
-        <div className='typewriter'>
-          <h1>{gameList.description}</h1>
-        </div>
+        <TypeWriterEffect
+          textStyle={{ fontFamily: "Red Hat Display" }}
+          startDelay={2000}
+          cursorColor="orange"
+          text={gameList.description}
+          typeSpeed={100}
+        />
+
         <GameStage />
       </DescriptionBox>
     </GameBox>
