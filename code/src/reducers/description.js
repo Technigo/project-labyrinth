9927530e12b0e23e0556d415ef5bf2024c4d2f25
HedvigM <hedvig@mejstedt.se>
2021-11-24 14:11:31 +0100

    Update description.js
    changed order on the fetches.

--- code/src/reducers/description.js
+++ code/src/reducers/description.js
@@ -1,18 +1,3 @@
-export const fetchContinue = (username, type, direction) => {
-  return (dispatch) => {
-    // dispatch borde vara här
-    fetch('https://wk16-backend.herokuapp.com/action', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: 'Owl' })
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        dispatch(labyrinth.actions.setDescription(json));
-        dispatch(labyrinth.actions.setDesciptionHistory({ direction }));
-      });
-  };
-};
 import { createSlice } from '@reduxjs/toolkit';
 
 export const labyrinth = createSlice({
@@ -39,6 +24,22 @@ export const fetchStart = () => {
       .then((res) => res.json())
       .then((json) => {
         dispatch(labyrinth.actions.setDescription(json));
+      });
+  };
+};
+
+export const fetchContinue = (username, type, direction) => {
+  return (dispatch) => {
+    // dispatch borde vara här
+    fetch('https://wk16-backend.herokuapp.com/action', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: 'Owl' })
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(labyrinth.actions.setDescription(json));
+        dispatch(labyrinth.actions.setDesciptionHistory({ direction }));
       });
   };
 };
