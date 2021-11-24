e0cca013e4c32b93dbff0ea42b12f13fe2e8e10c
vrill <52007698+vrill@users.noreply.github.com>
2021-11-24 17:47:52 +0100

    Added the history state, and renamed labytinthData to currentCoordinates tfor a more accurate description.

--- code/src/reducers/game.js
+++ code/src/reducers/game.js
@@ -1,39 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
 export const game = createSlice({
   name: 'game',
   initialState: {
     username: null,
-    labyrinthData: null,
-    loading: false
+    currentCoordinates: null,
+    history: [],
+    loading: false,
   },
   
   reducers: {
     setUsername: (store, action) => {
       store.username = action.payload
     },
-    setLabyrinthData: (store, action) => {
-      store.labyrinthData = action.payload
+    setCurrentCoordinates: (store, action) => {
+      store.currentCoordinates = action.payload
+    },
+    setHistory: (store, action) => {
+      if (store.currentCoordinates) {
+        store.history = [...store.history, action.payload]
+      }
     },
     setLoading: (store, action) => {
       store.loading = action.payload
-    }
-  }
+    },
+  },
 })
 
 //Sending a post request to get the data to generate the game 
 export const generateGame =  () => {
-  return (dispatch, getState) => {
+  return (dispatch, getState) => { // get the current state of the store, for values defined in initialState
     dispatch(game.actions.setLoading(true))
     fetch('https://wk16-backend.herokuapp.com/start', {
       method: 'POST',
-      headers: {"Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username: getState().game.username }),
       // Using getState() to get the value from the state variable username instead of passing it into the thunk as an argument
     })
     .then((res) => res.json())
-    .then(data => dispatch(game.actions.setLabyrinthData(data))) 
+    .then(data => dispatch(game.actions.setCurrentCoordinates(data))) 
     .finally(() => dispatch(game.actions.setLoading(false)))
   }
 }
@@ -43,7 +48,7 @@ export const nextStep = (type, direction) => {
     dispatch(game.actions.setLoading(true))
     fetch('https://wk16-backend.herokuapp.com/action', {
       method: 'POST',
-      headers: {"Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         username: getState().game.username,
         type,
@@ -52,8 +57,8 @@ export const nextStep = (type, direction) => {
     })
       .then(res => res.json())
       .then(data => {
-        dispatch(game.actions.setLabyrinthData(data))
-        /* dispatch(game.actions.setHistory(data)) */
+        dispatch(game.actions.setCurrentCoordinates(data))
+        dispatch(game.actions.setHistory(data))
       })
       .finally(() => dispatch(game.actions.setLoading(false)))
   }
