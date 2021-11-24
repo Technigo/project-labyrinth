c2eaa922a991a6b801da013e039e0d81628c67f6
Darya <darya.lapata@gmail.com>
2021-11-24 07:39:15 +0100

    added animation to loader and some styling, we can change that after

--- code/src/components/LoadingIndicator.js
+++ code/src/components/LoadingIndicator.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Lottie from "react-lottie";
-
 import { useSelector } from "react-redux";
+
+import Lottie from "react-lottie";
+import animationData from "../animations/83694-loading-double.json"; 
+import styled from 'styled-components'; 
 
 export const LoadingIndicator = () => {
   const loading = useSelector((state) => state.ui.isLoading);
@@ -9,6 +11,7 @@ export const LoadingIndicator = () => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
+		animationData,
     rendererSettings: {
     preserveAspectRatio: "xMidYMid slice"
     }
@@ -17,11 +20,28 @@ export const LoadingIndicator = () => {
 return (
   <> 
   {loading && 
-      <div>
-        <h1>Loading...</h1>
+      <Div>
+				<Text>Loading ...</Text>
         <Lottie options={defaultOptions} height={400} width={400} />
-      </div>
+      </Div>
   }
   </>
 );
 };
+
+const Div = styled.div`
+	background: #294380;
+	height: 100vh;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`
+
+const Text = styled.p`
+	color: #ffffff;
+	font-size: 20px;
+`
