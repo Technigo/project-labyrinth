149688d58dc6d2e4a03e92dc22c12dfcdf7c11e9
pdetli <74938586+pdetli@users.noreply.github.com>
2021-11-24 23:26:49 +0100

    background image styled

--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { labyrinth, continueLabyrinth } from "reducers/labyrinth"
 import { useSelector, useDispatch } from "react-redux"
+import styled from "styled-components"
+
 // import { GameStart } from "./GameStart"
 
 export const MainPage = () => {
   // const loading = useSelector((state) => state.ui.loading)
   const response = useSelector((state) => state.labyrinth.response)
+  // const { coordinates, actions } = useSelector(
+  //   (state) => state.labyrinth.response.actions
+  // )
+
   const actions = useSelector((state) => state.labyrinth.response.actions)
 
   const dispatch = useDispatch()
@@ -20,21 +26,74 @@ export const MainPage = () => {
     dispatch(labyrinth.actions.reStart())
   }
 
+  const Button = styled.button`
+    background-color: yellow;
+  `
+  const MainWrapper = styled.section`
+    background-image: url(./assets/forest.jpg);
+    background-size: cover;
+    height: 100vh;
+    width: 100wh;
+    text-align: center;
+    justify-content: center;
+    margin: auto 0;
+  `
+
+  // const setBgColor = () => {
+  //   let bg = "pink"
+  //   switch (coordinates) {
+  //     case "0,0":
+  //       bg = "red"
+  //       break
+  //     case "1,0":
+  //       bg = "green"
+  //       break
+  //     case "1,1":
+  //       bg = "blue"
+  //       break
+  //     case "0,1":
+  //       bg = "yellow"
+  //       break
+  //     case "0,2":
+  //       bg = "orange"
+  //       break
+  //     case "0,3":
+  //       bg = "brown"
+  //       break
+  //     case "1,3":
+  //       bg = "purple"
+  //       break
+  //     default:
+  //       bg = "peachypuff"
+  //   }
+  //   return bg
+  // }
+
+  //another way of doing it:
+  // const colorDictionary = {
+  //   "0,0": "red",
+  //   "1,0": "pink",
+  //   "1,1": "blue",
+  //   "0,1": "yellow",
+  //   "0,2": "purple",
+  //   "0,3": "green",
+  //   "1,3": "orange",
+  // }
   return (
     // loading === false &&  dont need this line since loading has its own logic inside the component
     <>
-      <div>
+      <MainWrapper>
         <h1>{response.description}</h1>
         {response.coordinates !== undefined && (
           <>
             <div>
               {actions.map((action) => (
                 <div key={action.description}>
-                  <button
+                  <Button
                     onClick={() => onButtonClick(action.type, action.direction)}
                   >
                     MOVE {action.direction.toUpperCase()}
-                  </button>
+                  </Button>
                   <p>{action.description}</p>
                 </div>
               ))}
@@ -45,7 +104,7 @@ export const MainPage = () => {
         {response.coordinates === "1,3" && (
           <button onClick={() => handleRestartButton()}> RESTART </button>
         )}
-      </div>
+      </MainWrapper>
     </>
   )
 }
