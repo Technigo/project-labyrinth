d58540a876a2394b42b1097e78099d60335552eb
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-11-24 18:26:37 +0100

    animation fixed?

--- code/src/components/GameBoard.js
+++ code/src/components/GameBoard.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { MainGame } from './MainGame';
-import { StartPage } from './StartPage';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { MainGame } from "./MainGame";
+import { StartPage } from "./StartPage";
+import { useSelector } from "react-redux";
 
 const GameBoard = () => {
   const game = useSelector((state) => state.game);
-  return <div>{game.username !== '' ? <MainGame /> : <StartPage />}</div>;
+  return <>{game.username !== "" ? <MainGame /> : <StartPage />}</>;
 };
 
 export default GameBoard;
