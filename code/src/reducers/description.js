bb1d2eda3304edf9932c67832304aed67bcb6210
HedvigM <hedvig@mejstedt.se>
2021-11-24 17:48:17 +0100

    start of the descrition page

--- code/src/reducers/description.js
+++ code/src/reducers/description.js
@@ -38,3 +38,19 @@ export const fetchStart = () => {
       .finally(() => dispatch(labyrinth.actions.setLoading(false)));
   };
 };
+
+export const fetchContinue = (username, type, direction) => {
+  return (dispatch) => {
+    // dispatch borde vara här
+    fetch('https://wk16-backend.herokuapp.com/action', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: 'Owl' })
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        dispatch(labyrinth.actions.setDescription(json));
+        dispatch(labyrinth.actions.setDesciptionHistory({ direction }));
+      });
+  };
+};
