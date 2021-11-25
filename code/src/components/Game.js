a475ad08c93fcf9b493a62c9a72631a491ba525b
Elin Elmvik Diczfalusy <elinelmvikdiczfalusy@Elins-iMac.local>
2021-11-25 19:51:06 +0100

    added switch and icons

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -5,6 +5,7 @@ import styled from "styled-components/macro"
 import { Starter } from "./Starter"
 import { moveMaze } from "../reducers/maze"
 import Loader from "./Loader"
+import { Rayoflight } from "lib/svg"
 
 /* import { NavButton } from "styling/styling" */
 
@@ -16,7 +17,7 @@ const AppMain = styled.section`
 const GameContainer = styled.section`
   max-width: 500px;
   padding: 30px;
-  background-color: rgba(255, 255, 255, 0.6);
+  background-color: rgba(255, 255, 255, 0.4);
   border: 1px solid black;
   border-radius: 10px;
 `
@@ -27,9 +28,7 @@ const InfoDiv = styled.div`
   border-radius: 10px;
   margin-bottom: 20px;
 `
-const NextDiv = styled.div`
-  text-align: center;
-`
+
 const NavDiv = styled.div`
   background-color: rgba(0, 0, 0, 0.7);
   color: white;
@@ -44,12 +43,13 @@ const ButtonContainer = styled.div`
   text-align: center;
 `
 const NavButton = styled.button`
-  background-color: lightblue;
+  //   background-color: lightblue;
   border: none;
   border-radius: 10px;
   max-width: 100px;
   padding: 10px;
   margin: 0 20px;
+  opacity: 1;
 `
 const TheEndDiv = styled.div`
   background-color: rgba(0, 0, 0, 0.7);
@@ -66,6 +66,62 @@ const Game = () => {
   console.log("username", userName) // CONSOLE
   console.log("data", data) // CONSOLE
 
+  const setBgColor = () => {
+    switch (data.coordinates) {
+      case "0,0":
+        return "#FADD57"
+
+      case "1,0":
+        return "#C0E5AC"
+
+      case "1,1":
+        return "#C3EEEE"
+
+      case "0,1":
+        return "#F0D5EB"
+
+      case "0,2":
+        return "#BCC5A3"
+
+      case "0,3":
+        return "#C0D0EE"
+
+      case "1,3":
+        return "#EAC082"
+
+      default:
+        return "#FADD57"
+    }
+  }
+
+  const setIcon = () => {
+    switch (data.coordinates) {
+      case "0,0":
+        return "./assets/img1.svg"
+
+      case "1,0":
+        return "./assets/pic1.svg"
+
+      case "1,1":
+        return "#C3EEEE"
+
+      case "0,1":
+        return "#F0D5EB"
+
+      case "0,2":
+        return "#BCC5A3"
+
+      case "0,3":
+        return "#C0D0EE"
+
+      case "1,3":
+        return "#EAC082"
+
+      default:
+        return "#FADD57"
+    }
+  }
+
   const dispatch = useDispatch()
 
   return (
@@ -75,21 +131,26 @@ const Game = () => {
       ) : (
         <GameContainer>
           <InfoDiv>
+            <img src={setIcon()}></img>
             <p>{data.description}</p>
             <p>Your coordinates are: {data.coordinates}</p>
+
+            {/* <Rayoflight /> */}
           </InfoDiv>
 
           {data.coordinates !== "1,3" ? (
             <>
               <NavDiv>
                 {data.actions.map(action => (
-                  <p key={action.direction}>{action.description}</p>
+                  <p key={action.direction}>
+                    {action.direction} - {action.description}
+                  </p>
                 ))}
               </NavDiv>
-
               <ButtonContainer>
                 {data.actions.map(action => (
                   <NavButton
+                    style={{ background: setBgColor() }}
                     key={action.description}
                     onClick={() =>
                       dispatch(moveMaze(action.direction, action.type))
