07a8a5e148f5b5950336dea09d1551bf964b2a35
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-11-25 09:41:02 +0100

    changed styling in game page and endpage

--- code/src/components/StyledComponents/DirectionButtons.js
+++ code/src/components/StyledComponents/DirectionButtons.js
@@ -4,12 +4,15 @@ export const DirectionButtons = styled.button`
 display: flex;
 flex-direction: row;
 margin: auto auto;
+padding: 5px 10px;
 border-radius: 10px;
 font-family: 'Philosopher', sans-serif;
 font-size: 16px;
+color: #fda;
 border:2px solid #fda; 
 margin-top: 20px;
 margin-bottom: 20px;
 cursor: pointer;
+background-color: #8A9A5B;
 
 `
