99d44deb25d0b7fb95118a6a7c9c92ca748950e0
Amanda Tilly <amandatilly@Amandas-2.lan>
2021-11-25 17:18:08 +0100

    styling and worked on loader

--- code/src/components/LoadingPage.js
+++ code/src/components/LoadingPage.js
@@ -3,8 +3,10 @@ import styled from "styled-components";
 
 const LoadingText = styled.h1`
   font-weight: bold;
-  font-size: 20px;
+  font-size: 36px;
   text-align: center;
+  font-family: "Langar", cursive;
+  letter-spacing: 2px;
 `;
 
 const Overlay = styled.div`
@@ -13,6 +15,8 @@ const Overlay = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+  width: 100%;
+  height: 100vh;
   background-color: #fff;
   display: flex;
   align-items: center;
