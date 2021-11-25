a59c7687005f5608add26494b01553d22261d95f
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2021-11-25 21:50:51 +0100

    implemented switch

--- code/src/components/Labyrinth.js
+++ code/src/components/Labyrinth.js
@@ -4,11 +4,43 @@ import { nextStep } from '../reducers/game';
 
 import './labyrinth.css';
 
+import '../assets/music.jpg';
+
 export const Labyrinth = () => {
   const { description, coordinates, actions } = useSelector(
     (store) => store.game.currentPosition
   );
   const dispatch = useDispatch();
+
+  const setBgImg = () => {
+    let bg = '';
+    switch (coordinates) {
+      case '0,0':
+        bg = '../assets/bookshelf.jpg';
+        break;
+      case '1,0':
+        bg = '../assets/ceiling.jpg';
+        break;
+      case '1,1':
+        bg = '../assets/colors.jpg';
+        break;
+      case '0,1':
+        bg = '../assets/soundwaves.jpg';
+        break;
+      case '0,2':
+        bg = '../assets/walls.jpg';
+        break;
+      case '0,3':
+        bg = '../assets/brigth-light.jpg';
+        break;
+      case '1,3':
+        bg = '../assets/cavern-arch.jpg';
+        break;
+      default:
+        bg = '../assets/music.jpg';
+    }
+    return bg;
+  };
 
   const handleButtonClick = (type, direction) => {
     //call the API, pass along type and dir as part of the body
@@ -26,11 +58,9 @@ export const Labyrinth = () => {
   );
 
   return (
-    <section>
-      <div className="description">
-        <p>{description}</p>
-        {/* <p>{coordinates}</p> */}
-      </div>
+    <section className="description" style={{ backgroundImage: setBgImg() }}>
+      <p>{description}</p>
+      <p>{coordinates}</p>
       {actions.length === 0 && <h3>Yay, you made it out!</h3>}
       {actions.length > 0 &&
         actions.map((item) => <ActionCard key={item.direction} {...item} />)}
