76eaeea3c6da3725bfb9a2fefd32e5009b1da9a9
HedvigM <hedvig@mejstedt.se>
2021-11-25 17:19:44 +0100

    styling startpage

--- code/src/components/LabyrinthGame.js
+++ code/src/components/LabyrinthGame.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { StartPage } from './StartPage';
 import { Description } from './Description';
 import { LoadingLottie } from './Lottie';
+import styled from 'styled-components';
 
 export const LabyrinthGame = () => {
   const currentPosition = useSelector(
@@ -10,9 +11,29 @@ export const LabyrinthGame = () => {
   );
 
   return (
-    <>
-      {currentPosition ? <Description /> : <StartPage />}
-      <LoadingLottie />
-    </>
+    <Container>
+      <InnerContainer>
+        {currentPosition ? <Description /> : <StartPage />}
+        <LoadingLottie />
+      </InnerContainer>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+
+  border: 2px solid pink;
+  padding: 50px;
+`;
+
+const InnerContainer = styled.div`
+  min-width: 334px;
+  max-width: 500px;
+  margin: 0 auto;
+  height: 100%;
+
+  border: 2px solid lime;
+`;
