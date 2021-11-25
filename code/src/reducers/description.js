004468ad6f53d0d5c017c4cb5777e52a38d838b7
HedvigM <hedvig@mejstedt.se>
2021-11-25 14:48:57 +0100

    Update description.js

--- code/src/reducers/description.js
+++ code/src/reducers/description.js
@@ -57,19 +57,3 @@ export const fetchContinue = (type, direction) => {
       .finally(() => dispatch(labyrinth.actions.setLoading(false)));
   };
 };
-
-export const fetchContinue = (username, type, direction) => {
-  return (dispatch) => {
-    // dispatch borde vara här
-    fetch('https://wk16-backend.herokuapp.com/action', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username: 'Owl' })
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        dispatch(labyrinth.actions.setDescription(json));
-        dispatch(labyrinth.actions.setDesciptionHistory({ direction }));
-      });
-  };
-};
