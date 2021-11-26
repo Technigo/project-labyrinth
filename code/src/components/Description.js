b3702526aaacc0288662e06deab5862673b51a08
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-11-26 01:47:14 +0100

    IMG change for each coordinate
    La inte in alla bilder, kollade bara så det funkade, hade inte länkar till alla bilder, har dem på min dator tänker att jag fixar URL för alla bilder imorgon

--- code/src/components/Description.js
+++ code/src/components/Description.js
@@ -4,11 +4,19 @@ import { fetchContinue } from '../reducers/description';
 import styled from 'styled-components';
 
 export const Description = () => {
-  const { description, actions } = useSelector(
+  const { description, coordinates, actions } = useSelector(
     (store) => store.labyrinth.currentPosition
   );
-
   const dispatch = useDispatch();
+
+  const colorDictionary = {
+    '0,0': 'url(https://images.unsplash.com/photo-1588571590960-9d6bfc172e01?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+    '1,1': 'blue',
+    '0,1': 'yellow',
+    '0,2': 'purple',
+    '0,3': 'green',
+    '1,3': 'url(https://images.unsplash.com/photo-1588571590960-9d6bfc172e01?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)',
+  }
 
   const onButtonClick = (type, direction) => {
     dispatch(fetchContinue(type, direction));
@@ -24,7 +32,7 @@ export const Description = () => {
   );
 
   return (
-    <section>
+    <section className='labyrinth' style={{ background: colorDictionary[coordinates] }}>
       <h1>{description}</h1>
       {actions.length === 0 && <h3>Yay you made it out!</h3>}
 
