cc1e8f5ee7b6eee56a48386badb6fe953a985b02
fatigr26 <fatigr2692@gmail.com>
2021-11-26 17:38:19 +0100

    fixed deploying errors

--- code/src/components/Start.js
+++ code/src/components/Start.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import img from "../assets/background-maze.jpeg";
 //Reducer game
