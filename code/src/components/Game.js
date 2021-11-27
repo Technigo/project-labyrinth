1dd2d1b7080af9a8eb9a665bda8f3b64fc74c81c
silvertejp89 <kriss.stockhaus@gmail.com>
2021-11-27 16:06:13 +0100

    removed unused code

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -3,14 +3,12 @@ import { useSelector } from "react-redux";
 
 import { Start } from "./Start";
 import { Labyrinth } from "./Labyrinth";
-import { LoadingIndicator } from "./LoadingIndicator";
 
 export const Game = () => {
   const currentPosition = useSelector((store) => store.game.currentPosition);
 
   return (
     <div className="container">
-      {/* <LoadingIndicator /> */}
       {currentPosition ? <Labyrinth /> : <Start />}
     </div>
   );
