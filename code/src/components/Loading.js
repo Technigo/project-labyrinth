165e48220dbde2027950defa4784a7e0a5b5c38c
Naushin Malik <nosheen.malik@gmail.com>
2021-11-29 10:06:23 +0100

    resulved some more issues for bulild

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,37 +1,41 @@
-import React from 'react'
-import Lottie from 'react-lottie'
-import animationData from '../animation/loader'
-import { useSelector } from 'react-redux'
-import styled from 'styled-components'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Lottie from 'react-lottie';
+import styled from 'styled-components';
+import animationData from '../animation/loader';
 
-//styling of components: used "parent" absolute and "child" relative, to make the spinner appear where we wanted it
+// styling of components: used "parent" absolute and "child" relative, to make the spinner appear where we wanted it
 const Section = styled.section`
-display: flex;
-width: 100%;
-height: 100vh;
-position: absolute;
-justify-content: center;
-align-items: center;
-background-color: #3c4f34;
-`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  background-color: #3c4f34;
+`;
 const Spinner = styled.div`
-position: relative; 
-`
+  position: relative;
+`;
 const LoadingAnimation = () => {
-    const loading = useSelector ((store) => store.animation.loading)
-    const defaultOptions = {
-        loop: true,
+  const loading = useSelector((store) => store.animation.loading);
+  const defaultOptions = {
+    loop: true,
     autoplay: true,
     animationData,
     rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-    }
-}
-return (
-    <Section>   
-        <Spinner>   
-        {loading && <Lottie options={defaultOptions} height={325} width={325} />} {/*changed height and width to better fit all devises*/}
-        </Spinner>
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+  return (
+    <Section>
+      <Spinner>
+        {loading && (
+          <Lottie options={defaultOptions} height={325} width={325} />
+        )}{' '}
+        {/*changed height and width to better fit all devises*/}
+      </Spinner>
     </Section>
-)}
+  );
+};
 export default LoadingAnimation;
