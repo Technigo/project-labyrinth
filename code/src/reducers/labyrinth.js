34879a9ae0a9c727445927dc2e99a81dd9e415f0
Naushin Malik <nosheen.malik@gmail.com>
2021-11-29 10:12:24 +0100

    removed the redundant code

--- code/src/reducers/labyrinth.js
+++ code/src/reducers/labyrinth.js
@@ -1,47 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { animation } from './animation'
+import { animation } from './animation';
 
-//this is for the labyrinth-game
+// this is for the labyrinth-game
 export const labyrinth = createSlice({
-    name: 'labyrinth',
-    initialState: {
+  name: 'labyrinth',
+  initialState: {
     destination: [],
-    history: [],
     userName: '',
-    },
-    reducers: {
+  },
+  reducers: {
     setLabyrinthPath: (state, action) => {
-        state.destination = action.payload;
+      state.destination = action.payload;
     },
-    addUserName: (state, action) =>{
-        state.userName = action.payload
-    },
-    setHistory: (state, action) => { {/*this is not used, due to lack of time. If we had more time we would have implemented it further.. */}
-        state.history = [...state.history, action.payload] 
+    addUserName: (state, action) => {
+      state.userName = action.payload;
     },
     restartLabyrinth: (state) => {
-        state.destination = []
-        state.userName = ''
+      state.destination = [];
+      state.userName = '';
     },
-}}  
-);
-
-export const fetchLabyrinth = ({url, type, direction}) => { // this is passed along, depending on what user clicks on
-    return (dispatch, getState) => {
-    dispatch (animation.actions.setLoading(true))
+  },
+});
+// this is passed along, depending on what user clicks on
+export const fetchLabyrinth = ({ url, type, direction }) => {
+  return (dispatch, getState) => {
+    dispatch(animation.actions.setLoading(true));
     fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 
-            username: getState().labyrinth.userName,
-            type,
-            direction,
-        }),
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: getState().labyrinth.userName,
+        type,
+        direction,
+      }),
     })
-        .then((res) => res.json())
-        .then((json) => {
+      .then((res) => res.json())
+      .then((json) => {
         dispatch(labyrinth.actions.setLabyrinthPath(json));
-        dispatch (animation.actions.setLoading(false)) //we can type .finally id useHistory is added at a later stage
-        });
-    };
+        // we can type .finally id useHistory is added at a later stage
+        dispatch(animation.actions.setLoading(false));
+      });
+  };
 };
