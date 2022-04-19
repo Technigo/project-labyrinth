a6075620dd501d0ba4004164f0cf39908fe1095f
Arianna Allahyar <anallahyar@gmail.com>
2022-04-19 09:59:26 +0200

    Initial commit

--- code/src/index.js
+++ code/src/index.js
@@ -1,6 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { App } from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import instructions from "./reducers/instructions";
+
+const reducer = combineReducers({
+  instructions: instructions.reducer,
+});
+
+const store = configureStore({ reducer });
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ code/src/reducers/instructions.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const instructions = createSlice({
+  name: "instructions",
+  initialState: {},
+  reducers: {},
+});
+
+export default instructions;
