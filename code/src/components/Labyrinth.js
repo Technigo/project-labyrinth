5f35c3c8de3300c3690f8387355861ab2cef2d67
Nhung Phan <nhungphan@Joel-Lov.local>
2022-04-20 00:33:07 +0300

    added button controllers

--- code/src/components/Labyrinth.js
+++ code/src/components/Labyrinth.js
@@ -7,6 +7,26 @@ const Background = styled.div`
 	background: ${(props) => props.background};
 `
 
+const ButtonController = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3,1fr);
+	grid-template-rows: repeat(3,1fr);
+
+	background: white;
+	width: 60px;
+	height: 60px;
+
+
+`
+
+const Btn = styled.button`
+
+		background: none;
+		border: none;
+
+
+`
+
 /*const Wrapper = styled.div`
 	display: flex;
 	flex-direction: row;
@@ -54,31 +74,48 @@ const Background = styled.div`
 const Labyrinth = () => {
 	const [select, setSelect] = useState([])
 	const [nav, setNav] = useState('')
-	const [directionBtn, setDirectionBtn] = useState('')
 	const dispatch = useDispatch()
 	const items = useSelector((store) => store.game.items)
 
+	let arr = [];
+	const dir = items.actions.map(item => arr.push(item.direction))
+
+	console.log(arr)
 	const onNavigate = (type, direction) => {
 		setSelect((prev) => [...prev, direction])
-		setNav(direction)
 		dispatch(navigateGame(type, direction))
 	}
-	console.log(nav)
 
 	const onRestartClick = () => {
 		dispatch(game.actions.restartGame())
 	}
 
-	const btnNavigator = ['North', 'East', 'South', 'West']
-	//const btnDirections = items.actions.map(item => item.)
+	const btnNavigator = ['North', 'East', 'South', 'West'];
 
-	const filterBtn = (activeItem) => {
-		btnNavigator.map((item) => {
-			if (item !== activeItem) {
-				return <button>{item} </button>
-			}
-		})
+	const changeDirectionToIcon = (way) => {
+		if (way === 'North') {
+			return '⬆️'
+		} else if (way === 'South') {
+			return '⬇️'
+		} else if (way === 'East') {
+			return '➡️'
+		} else if (way === 'West'){
+			return '⬅️'
+		}
 	}
+	
+
+
+
+	const filterDisableBtn = () => {
+
+		const newOne = btnNavigator.filter(item => !arr.includes(item))
+		
+		return newOne.map(item => <Btn disabled>{changeDirectionToIcon(item)}</Btn>)
+	}
+
+	console.log(dir)
+
 	const changeBG = () => {
 		switch (items.coordinates) {
 			case '0,0':
@@ -115,18 +152,22 @@ const Labyrinth = () => {
 					<div>
 						<p>{item.description}</p>
 
-						{/*<button onClick={() => onNavigate(item.type, item.direction)}>{item.direction}</button>*/}
 					</div>
 				)
-			})}
-			{items.actions.map((item) => {
-				return (
-					<>
-						{filterBtn(item.direction)}
-						<button onClick={() => onNavigate(item.type, item.direction)}>{item.direction}</button>
-					</>
-				)
-			})}
+			})} 
+
+			<ButtonController>
+
+				{items.actions.map((item) => {
+					return (
+						<>
+							<Btn onClick={() => onNavigate(item.type, item.direction)}>{changeDirectionToIcon(item.direction)}</Btn>
+						</>
+					)
+				})}
+				{filterDisableBtn()}
+
+			</ButtonController>
 
 			<button type='button' onClick={onRestartClick}>
 				{items.coordinates === '1,3' ? 'play again' : 'restart game'}
