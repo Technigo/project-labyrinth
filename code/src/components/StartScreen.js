5d64dce40d0227cf34523d4966c079912a1451fe
Emma Lindell <emma.lindell4@gmail.com>
2022-04-19 20:04:22 +0200

    Added inputValue to input

--- code/src/components/StartScreen.js
+++ code/src/components/StartScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 
 const StartScreen = () => {
@@ -7,22 +7,24 @@ const StartScreen = () => {
         event.preventDefault()
     }
 
+    const [inputValue, setInputValue] = useState('')
     
     return (
-    
-    <>
-        <section className="first-page">
+
+        <section>
         <h1>Want to enter the labyrinth?</h1>
 
         <form onSubmit={handleSubmit}>
         <label>
             Add your username:
-            <input type="text" />
+            <input 
+            type="text"
+            value={inputValue}
+            onChange={event => setInputValue(event.target.value)} />
         </label>
         <button type="submit">Let's play</button>
         </form>
-        </section>       
-    </>
+        </section>
 
     )
 
