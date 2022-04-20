b2e6deb15a2305ee1b00cff5410a6d7d80cd9889
JensBergqvist <jensabb@hotmail.com>
2022-04-20 18:45:06 +0200

    first fetch working

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+
+fetch(`https://labyrinth-technigo.herokuapp.com/start`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username: "technigoplayar" }),
+})
+  .then((res) => res.json())
+  .then((res) => console.log(res));
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+  return <div>Find me in src/app.js!</div>;
+};
