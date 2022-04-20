10d30cb709cb23c0689ff6c7459939ebc8ba9c40
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-04-20 15:29:01 +0200

    next step fix

--- code/src/components/Gamepage.js
+++ code/src/components/Gamepage.js
@@ -1,23 +1,55 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
-//import { nextStep } from "reducers/labyrinth";
+import { useSelector, useDispatch } from "react-redux";
+import { loading, Loading } from "./Loading";
+import { nextStep } from "reducers/labyrinth";
 
 const Gamepage = () => {
-  const { act}
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  const gamedescription = useSelector(
+  const description = useSelector(
     (store) => store.labyrinth.gameStatus.description
   );
-  const gamecoordinates = useSelector(
+  const coordinates = useSelector(
     (store) => store.labyrinth.gameStatus.coordinates
   );
   const actions = useSelector((store) => store.labyrinth.gameStatus.actions);
 
+  const handleClick = (type, direction) => {
+    dispatch(nextStep(type, direction));
+  };
+
+  const GameCard = ({ description, type, direction }) => (
+    <>
+      <p>{description}</p>
+      <>
+        <button onClick={() => handleClick(type, direction)}>
+          {type} {direction}
+        </button>
+      </>
+    </>
+  );
   return (
-    <div>
-      <h1>{gamedescription}</h1>
+    <>
+      <div>
+        {loading && <Loading />}
+        {!loading && (
+          <div>
+            {actions.length !== 0 && (
+              <>
+                <h1>{description}</h1>
+                <p>Your position is {coordinates}</p>
+              </>
+            )}
+            {actions.length === 0 ? (
+              <div>This is the end of the game</div>
+            ) : (
+              actions.length > 0 &&
+              actions.map((item) => <GameCard {...item} />)
+            )}
+          </div>
+        )}
+      </div>
+      {/* <h1>{gamedescription}</h1>
       <h2>you are at coordinates {gamecoordinates}</h2>
       {actions.map((action) => {
         return (
@@ -28,8 +60,8 @@ const Gamepage = () => {
             </button>
           </div>
         );
-      })}
-    </div>
+      })} */}
+    </>
   );
 };
 
