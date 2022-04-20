6625a88bc4f4edf9aae396ef10a5d134f64e8858
Emma Lindell <emma.lindell4@gmail.com>
2022-04-20 20:09:55 +0200

    added to master

---
+++ code/src/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const GameArea = styled.body`
+  background-image: url("https://images.pexels.com/photos/1731660/pexels-photo-1731660.jpeg?cs=srgb&dl=pexels-boris-ulzibat-1731660.jpg&fm=jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  row-gap: 30px;
+  position: absolute;
+` 
+
