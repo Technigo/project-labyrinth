0b0a230540aae799e0bdb03e4a446ed507291a43
Emma Högberg <emmahogberg88@gmail.com>
2022-04-20 22:48:03 +0200

    Bug fixes

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -4,11 +4,34 @@ import styled from 'styled-components/macro'
 
 import game, { fetchGame } from '../reducer/game'
 
+const ConsoleContent = styled.section`
+	background: #fff;
+	height: 472px;
+	width: 93%;
+	border-radius: 10px;
+	border: 2px solid;
+`
+const GameText = styled.p`
+	font-size: 16px;
+`
+const UsernameInput = styled.input`
+	height: 25px;
+`
+const UsernameInputContainer = styled.div`
+	display: flex;
+	justify-content: center;
+`
+
 const Game = () => {
 	const [userName, setUserName] = useState('')
 
 	const dispatch = useDispatch()
-	console.log('game')
+	console.log('username', userName)
+
+	const onInputUserName = (event) => {
+		event.preventDefault()
+		setUserName(event.target.value)
+	}
 
 	const onChangeUserName = () => {
 		dispatch(game.actions.setName(userName))
@@ -16,29 +39,11 @@ const Game = () => {
 		dispatch(fetchGame())
 	}
 
-	const ConsoleContent = styled.section`
-		background: #fff;
-		height: 472px;
-		width: 93%;
-		border-radius: 10px;
-		border: 2px solid;
-	`
-	const GameText = styled.p`
-		font-size: 16px;
-	`
-	const UsernameInput = styled.input`
-		height: 25px;
-	`
-	const UsernameInputContainer = styled.div`
-		display: flex;
-		justify-content: center;
-	`
-
 	return (
 		<ConsoleContent>
 			<GameText> Welcome to the a-maze-ing 80's! Try to found your way out of the labyrinth!</GameText>
 			<UsernameInputContainer>
-				<UsernameInput type='text' value={userName} onChange={(e) => setUserName(e.target.value)} />
+				<UsernameInput type='text' value={userName} onChange={onInputUserName} />
 				<button onClick={onChangeUserName}>Start</button>
 			</UsernameInputContainer>
 		</ConsoleContent>
