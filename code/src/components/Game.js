7b35d4054fb9a1c98fec1a1f7b563740321087f3
joannaringqvist <joanna.ringqvist@gmail.com>
2022-04-20 09:58:00 +0200

    Restructured a bit and added some styling

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import styled from 'styled-components/macro'
+// import styled from 'styled-components/macro'
 
 import StartScreen from './StartScreen'
 import GameScreen from './GameScreen'
 import Loader from './Loader'
-import {
-  bg0_0,
-  bg0_1,
-  bg0_2,
-  bg0_3,
-  bg1_0,
-  bg1_1,
-  bg1_3,
-  bg_start
-} from 'styles/BackgroundURLs'
+// import {
+//   bg0_0,
+//   bg0_1,
+//   bg0_2,
+//   bg0_3,
+//   bg1_0,
+//   bg1_1,
+//   bg1_3,
+//   bg_start
+// } from 'styles/BackgroundURLs'
 
 export const Game = () => {
 
   const username = useSelector(store => store.labyrinth.username)
-  const coordinates = useSelector(store => store.labyrinth.currentStep.coordinates)
+  // const coordinates = useSelector(store => store.labyrinth.currentStep.coordinates)
   const isLoading = useSelector((store) => store.ui.isLoading)
 
   if (isLoading) {
@@ -27,36 +27,36 @@ export const Game = () => {
   }
 
   return (
-    <Section coordinates={coordinates}>
+    <main>
       {username ?
         <GameScreen />
         :
         <StartScreen />}
-    </Section>
+    </main>
   )
 }
 
 export default Game
 
-const Section = styled.section`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
-  background-image: ${(props) =>
-    props.coordinates === '0,0'
-      ? bg0_0
-      : props.coordinates === '0,1'
-        ? bg0_1
-        : props.coordinates === '0,2'
-          ? bg0_2
-          : props.coordinates === '0,3'
-            ? bg0_3
-            : props.coordinates === '1,0'
-              ? bg1_0
-              : props.coordinates === '1,1'
-                ? bg1_1
-                : props.coordinates === '1,3'
-                  ? bg1_3 :
-                  bg_start};
-`
+// const Section = styled.section`
+//   display: flex;
+//   height: 100vh;
+//   width: 100vw;
+//   background-size: cover;
+//   background-image: ${(props) =>
+//     props.coordinates === '0,0'
+//       ? bg0_0
+//       : props.coordinates === '0,1'
+//         ? bg0_1
+//         : props.coordinates === '0,2'
+//           ? bg0_2
+//           : props.coordinates === '0,3'
+//             ? bg0_3
+//             : props.coordinates === '1,0'
+//               ? bg1_0
+//               : props.coordinates === '1,1'
+//                 ? bg1_1
+//                 : props.coordinates === '1,3'
+//                   ? bg1_3 :
+//                   bg_start};
+// `
--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -3,40 +3,90 @@ import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components/macro'
 
 import labyrinth, { continueGame } from 'reducers/labyrinth'
+import {
+  bg0_0,
+  bg0_1,
+  bg0_2,
+  bg0_3,
+  bg1_0,
+  bg1_1,
+  bg1_3,
+  bg_start
+} from 'styles/BackgroundURLs'
+
 
 export const GameScreen = () => {
   const dispatch = useDispatch()
+
+  const coordinates = useSelector(store => store.labyrinth.currentStep.coordinates)
 
   const currentStep = useSelector(store => store.labyrinth.currentStep)
   const currentStepActions = useSelector(store => store.labyrinth.currentStep.actions)
   const history = useSelector(store => store.labyrinth.history)
 
   return (
-    <StepDiv>
-      <h3>{currentStep.description}</h3>
+    <Section>
+      <MainDescription coordinates={coordinates}>
+        <h3>{currentStep.description}</h3>
+      </MainDescription>
+      <ActionsBox>
       {currentStepActions.map(action => (
-        <div key={action.direction}>
-          <p>To the {action.direction}: {action.description}</p>
+        <Action key={action.direction}>
           <button onClick={() => dispatch(continueGame(action.direction))}>Go {action.direction}</button>
-        </div>
+          <p>To the {action.direction}: {action.description}</p>
+        </Action>
       ))}
+      </ActionsBox>
       {history.length ?
-        <div>Your moves: {history.map((move, index) => (
+        <History>Your moves: {history.map((move, index) => (
           <span key={index}> ⇒ {move}</span>
-        ))}</div>
+        ))}</History>
         :
         ''}
       {!currentStepActions.length &&
         <button onClick={() => dispatch(labyrinth.actions.restartGame())}>Restart</button>}
-    </StepDiv>
+    </Section>
   )
 }
 
 export default GameScreen
 
-const StepDiv = styled.div`
+const Section = styled.section`
+  display: flex;
   height: 500px;
   width: 300px;
-  background-color: rgb(230, 230, 230, 0.5);
-  border: 2px solid red;
+  flex-direction: column;
+`
+
+const MainDescription = styled.div`
+  background-size: cover;
+    background-image: ${(props) =>
+      props.coordinates === '0,0'
+        ? bg0_0
+        : props.coordinates === '0,1'
+          ? bg0_1
+          : props.coordinates === '0,2'
+            ? bg0_2
+            : props.coordinates === '0,3'
+              ? bg0_3
+              : props.coordinates === '1,0'
+                ? bg1_0
+                : props.coordinates === '1,1'
+                  ? bg1_1
+                  : props.coordinates === '1,3'
+                    ? bg1_3 :
+                    bg_start};
+`
+
+const ActionsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Action = styled.div`
+  display: flex;
+`
+
+const History = styled.div`
+  display: flex;
 `
