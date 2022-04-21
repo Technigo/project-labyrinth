299c33d9cb4d04f8f939abf5f58b191558a0883d
Jessica Sandler <jessica.sandler@hotmail.com>
2022-04-21 20:04:42 +0200

    Add Lottie to LoadingPage.js

--- code/src/components/LoadingPage.js
+++ code/src/components/LoadingPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Lottie from 'react-lottie'
 import styled from 'styled-components/macro'
 import { useSelector } from 'react-redux'
+import animationData from '../lotties/loader'
 
 const LoaderContainer = styled.div`
   background-color: black;
   height: 100vh;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   color: lime;
@@ -16,11 +18,21 @@ const LoaderContainer = styled.div`
 const LoadingPage = () => {
   const isLoading = useSelector((state) => state.game.loading)
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <>
       {isLoading &&
         <LoaderContainer>
           <h1>Loading...</h1>
+          <Lottie options={defaultOptions} height={200} width={200} />
         </LoaderContainer>
       }
     </>
