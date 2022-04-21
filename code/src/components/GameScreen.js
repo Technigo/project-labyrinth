bb882c8703921b4dad01749e9933129e1ef25b34
JensBergqvist <jensabb@hotmail.com>
2022-04-21 16:28:09 +0200

    Blue level working

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -5,7 +5,7 @@ import { playGame } from "reducers/game";
 const GameScreen = () => {
   const gameData = useSelector((store) => store.game.gameData);
   //   const history = useSelector((store) => store.game.history);
-  const username = useSelector((store) => store.game.username);
+  //   const username = useSelector((store) => store.game.username);
 
   const dispatch = useDispatch();
 
@@ -24,24 +24,12 @@ const GameScreen = () => {
 
   return (
     <div>
-      <h1> Welcome {username}</h1>
+      {/* <h1> Welcome {username}</h1> */}
       <h3>Now the adventure begins</h3>
       <h3>{gameData.description}</h3>
       {gameData.actions.map((item) => (
         <ActionCard key={item.direction} {...item} />
       ))}
-      {/* <button disabled={!history.length} onClick={onDescriptionRevert}>
-        Go Back
-      </button>
-      <h3>This is a quote : {currentPosition.description}</h3>
-      {currentPosition.direction.map((direction) => (
-        <button
-          key={direction}
-          onClick={() => onDescriptionRegenerate(direction)}
-        >
-          {direction}
-        </button>
-      ))} */}
     </div>
   );
 };
--- code/src/components/tillfällig.js
+++
@@ -1,7 +0,0 @@
-fetch(`https://labyrinth-technigo.herokuapp.com/start`, {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ username: "technigoplayar" }),
-})
-  .then((res) => res.json())
-  .then((res) => console.log(res));
