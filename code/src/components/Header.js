06d40d8e0d159157025e845f7cc96ba677bf08dd
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-04-22 13:35:28 +0300

    added styling

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -3,19 +3,30 @@ import styled from "styled-components"
 
 const HeaderStyle = styled.header`
 display: flex;
-top: 50px;
+padding-top: 1rem;
 text-align: center;
 justify-content: center;
 `
 
 const Title = styled.h1`
-font-size: 30px;
+font-size: 1.5rem;
+padding:0;
+margin: 0;
+font-family: 'Monoton', cursive;
+
+@media (min-width: 768px) {
+    font-size: 2rem;
+  }
+@media (min-width: 1000px) {
+    font-size: 2.5rem;
+  }
+
 `
 
 export const Header = () => {
     return(
         <HeaderStyle>
-            <Title>Cave labyrinth</Title>
+            <Title>LABYRINTH</Title><img src="https://img.icons8.com/external-others-pike-picture/50/000000/external-maze-greece-others-pike-picture.png"/><Title>GAME</Title>
         </HeaderStyle>
     )
 }
--- code/src/components/Labyrinth.js
+++ code/src/components/Labyrinth.js
@@ -8,71 +8,94 @@ import { Game } from "./Game"
 import arrow from "../assets/compass.svg"
 
 const Section = styled.section`
-position: relative;
-width: 80%;
+position:relative;
+width: 70%;
 height: 100%;
-max-width: 400px;
+max-width: 500px;
 display: flex;
 flex-direction: column;
-justify-content: space-evenly;
+justify-content: center;
 align-items: center;
-padding: 20px;
-margin: auto;
+margin: 100px auto 0 auto;
+
 `
 const Title = styled.h1`
+
 align-text: center;
+font-size: 0.5rem;
+background-color: rgba(0,0,0, 0.5);
+padding: 10px;
+margin:auto;
+border-radius:10px;
+
+
+@media (min-width: 768px) {
+    font-size: 0.75rem;
+  }
+@media (min-width: 1000px) {
+    font-size: 1rem;
+  }
 
 `
 const NorthButton = styled.button`
 position: absolute;
 display: flex;
 flex-direction: column;
-width: 50px;
+width: 30px;
 background: transparent;
 border: none;
 color: white;
-top: -10%;
+top: 0;
 left: 50%;
+transform: rotate(0) translate(-50%, -50%);
 `
 const WestButton = styled.button`
 position: absolute;
-left: -15%;
+left: 0;
 top: 50%;
-transform: rotate(-90deg);
+transform: translate(-50%, -50%) rotate(-90deg);
 display: flex;
 flex-direction: column;
-width: 50px;
+width: 30px;
 background: transparent;
 border: none;
 color: white;
+
 `
 const SouthButton = styled.button`
 position: absolute;
-bottom: -10%;
+bottom: 0;
 left: 50%;
-transform: rotate(180deg);
+transform: translate(-50%, -50%) rotate(180deg);
 display: flex;
 flex-direction: column;
-width: 50px;
+width: 30px;
 background: transparent;
 border: none;
 color: white;
 `
 const EastButton = styled.button`
 position: absolute;
-right: -15%;
+right: 0;
 top: 50%;
-transform: rotate(90deg);
+transform: translate(-50%, -50%) rotate(90deg) ;
 display: flex;
 flex-direction: column;
-width: 50px;
+width: 30px;
 background: transparent;
 border: none;
 color: white;
 `
 const Arrow = styled.img`
 width: 100%;
+transition: transform 1s;
+cursor:pointer;
+
+&:hover{
+    transform: scale(1.5);
+}
 `
+
 
 export const Labyrinth = () => {
     const {description, coordinates, actions} = useSelector(store => store.game.currentPosition)
@@ -93,7 +116,7 @@ export const Labyrinth = () => {
             {direction === "North" && <NorthButton onClick={() => handleButtonClick(type, direction)}><Arrow src={arrow}/></NorthButton>}
             {direction === "West" && <WestButton onClick={() => handleButtonClick(type, direction)}><Arrow src={arrow}/></WestButton>}
             {direction === "South" && <SouthButton onClick={() => handleButtonClick(type, direction)}><Arrow src={arrow}/></SouthButton>}
-            {direction === "East" && <EastButton onClick={() => handleButtonClick(type, direction)}><Arrow src={arrow}/></EastButton>}
+            {/* {direction === "East" && <EastButton onClick={() => handleButtonClick(type, direction)}><Arrow src={arrow}/></EastButton>} */}
             {/* <p>{description}</p> */}
         </div>
     )
@@ -104,7 +127,7 @@ export const Labyrinth = () => {
         <Section>
             <Title>{description}</Title>
 
-            {actions.length === 0 && <div><button onClick={() => Restart()}>Play again</button> </div>}
+            {actions.length === 0 && <div><p>You found your way out, congrats!</p><button onClick={() => Restart()}>Play again</button> </div>}
 
             {actions.length > 0 && actions.map(item => <ActionCard key={item.direction} {...item} />)}
             
