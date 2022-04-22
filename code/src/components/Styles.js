08cd18f6b613cc4d38ca8de7979b3beee6546954
Louisa <lundislon@gmail.com>
2022-04-22 19:29:02 +0200

    test for responsiveness styling

--- code/src/components/Styles.js
+++ code/src/components/Styles.js
@@ -1,6 +1,11 @@
 
 import styled from 'styled-components/macro'
 
+const devices = {
+    tablet: "(min-width: 768px)",
+    desktop: "(min-width: 1025px)",
+  };
+
 export const Container = styled.div`
     display: flex;
 	align-items: center;
@@ -12,6 +17,14 @@ export const Container = styled.div`
  export const ChildContainer = styled.div`
  margin: 0 20px;
  font-size: 10px;
+
+ @media ${devices.tablet} {
+    width: 50%;
+  }
+  @media ${devices.desktop} {
+    width: 50%;
+  }
+
  `
  export const MazeCardText = styled.div`
   margin-bottom: 3rem;
