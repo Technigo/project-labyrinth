6f90602f4014e927cf86924dab062383029a849f
EmmaaBerg <emmaberg0819@gmail.com>
2022-04-22 15:32:32 +0200

    fixed deploy bug

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import styled from 'styled-components/macro'
 
 import LoaderAnimation from './animation'
 
-/*const LoaderAnimation = styled.p`
-  background-color: #000;
-  
-`*/
-
-//store.ui för att nå den slicen
-//store.ui.isLoading då det är initialState som då är antingen falsk till att börja med
-//men när api (fetchen) hämtas kommer det ta 1 sek cirkus och då finns det inget att mappa
 
 const Loading = () => {
     const isLoading = useSelector((store) => store.ui.isLoading);
