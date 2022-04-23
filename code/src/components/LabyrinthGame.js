8c1deef789fa5c12fa5ef8ca3fdc17e891a66c10
Jacob <jacob@engd.se>
2022-04-23 09:46:19 +0200

    Added styled component - LabyrinthGame

--- code/src/components/LabyrinthGame.js
+++ code/src/components/LabyrinthGame.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 
+import styled from 'styled-components';
+
 import StartScreen from './StartScreen'
 import GameScreen from './GameScreen'
 
+const LabyrithgameWrapper = styled.section`
+display: flex;
+flex-direction: column;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  position: absolute;
+
+`
 const LabyrinthGame = () => {
 
   const startGame = useSelector((state) => state.game.username);
@@ -11,8 +25,10 @@ const LabyrinthGame = () => {
   return (
     //conditional rendering här? om username === null, startscreen, else gamescreen?
     <>
+    <LabyrithgameWrapper>
     {!startGame && <StartScreen />}
     {startGame && <GameScreen />}
+    </LabyrithgameWrapper>
     </>
   )
 }
