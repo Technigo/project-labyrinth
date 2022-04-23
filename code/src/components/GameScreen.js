a03ece2ebbcba19bec8c78ecd1a59728d69f49fd
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-04-23 11:40:57 +0200

    Display api response (description, direction) on UI

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -16,28 +16,9 @@ const GameScreen = () => {
 
     const description = useSelector(state => state.game.game.description)
     const actions = useSelector(state => state.game.game.actions)
-    //1. Want to display the description of each ACTION property, but fails the page on initial render due to game array empty initially
-    //const actionDescription = useSelector(state => state.game.game.actions.description)
+
     console.log(description)
     console.log(actions)
-
-    if (actions) {
-      console.log('actions exist now')
-      //2. Thus thought to render only if actions equals to true (exists). But cannot conditionally render useselector 
-      //const actionDescription = useSelector(state => state.game.game.actions.description)
-      //cannot put useselector inside here to be called conditionally (rules of hooks) - breaks the code
-      //console.log(actionDescription)
-      //3. Another solution could be to map over the actions array once it exists:
-      actions.map(content => {
-        return (
-          console.log(content.direction),
-          console.log(content.description)
-        )
-      })
-      //4. How do we display this in the UI though?
-    } else {
-      console.log('actions do not exist yet')
-    }
 
     const onChooseDirection = (index) => {
       setDirection(directionArray[index])
@@ -52,7 +33,15 @@ const GameScreen = () => {
   return (
       <div>
           <p>{description}</p>
-          <p>{direction}</p>
+          {direction !== '' && <p>You chose to go: {direction}</p>}          
+          {actions && 
+          actions.map(content => 
+              <>
+              <p style={{fontWeight: 'bold'}}>Alternative way:</p>
+              <p>{content.direction}</p>
+              <p>{content.description}</p>
+              </>)
+          }          
           {eastBtn}
           {southBtn}
           {westBtn}
