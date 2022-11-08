30d8a10172de1f8754a50e8b5cfe34e74ba3caaf
Amanda Elvkull <108291356+AmandaElvkull@users.noreply.github.com>
2022-11-08 10:36:29 +0100

    installed packegs and not found page lottie

---
+++ code/src/components/Wrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NotFound } from './NotFound';
+
+const Wrapper = () => {
+    return (
+        <>
+        <NotFound />
+        </>
+    )
+}
+
+export default Wrapper
