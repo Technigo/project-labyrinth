e7ccdf50e9200a3495395cb4db0136feec648a96
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2022-11-08 13:45:50 +0100

    Tuesday's code along with Daniel

---
+++ code/src/components/AuthorInputSearch.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import quotes, { generateQuote } from 'reducers/quotes';
+
+const AuthorInputSearch = () => {
+  const [authorNameInputValue, setAuthorNameInputValue] = useState('');
+  const dispatch = useDispatch();
+  // const author = useSelector((store) => store.quotes.author);
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(quotes.actions.setAuthor(authorNameInputValue));
+    dispatch(generateQuote());
+  }
+
+  return (
+    <form onSubmit={(event) => onFormSubmit(event)}>
+      <label htmlFor="author-input">
+      Provide author name, please:
+        <input id="author-input" type="text" onChange={(event) => setAuthorNameInputValue(event.target.value)} />
+      </label>
+      <button type="submit">search for quotes</button>
+    </form>)
+}
+
+export default AuthorInputSearch;
