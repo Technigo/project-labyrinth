9cec9d912916bba4773660f9265f1c44bc70df6e
a-well <amandaemkwellington@gmail.com>
2022-11-09 23:16:13 +0100

    Randomise username

--- code/src/Components/UserNameInput.js
+++ code/src/Components/UserNameInput.js
@@ -2,8 +2,20 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import game, { generateDescription } from 'reducers/game';
 import styled from 'styled-components';
+import Header from './Header';
+import Wrapper from './Wrapper';
 
 // @TODO generate random unique username to always make game work?
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const UserNameInput = () => {
   const [userNameInputValue, setUserNameInputValue] = useState('');
@@ -12,46 +24,42 @@ const UserNameInput = () => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    dispatch(game.actions.setUser(userNameInputValue)); // klar
+    dispatch(game.actions.setUser(userNameInputValue + makeid(16))); // klar
     dispatch(generateDescription());
     userNameInputValue('');
   }
 
   return (
-    <Container>
-      <h1>The Maze game</h1>
-      <UserNameInputContainer onSubmit={(event) => onFormSubmit(event)}>
-        <label htmlFor="user-input">
-          Enter your name to start the game:
-          <input
-            id="user-input"
-            type="text"
-            value={userNameInputValue}
-            required
-            // eslint-disable-next-line jsx-a11y/no-autofocus
-            autoFocus
-            onChange={(event) => setUserNameInputValue(event.target.value)}
-            placeholder="Name..." />
-        </label>
-        <StartButton type="submit">Start game</StartButton>
-      </UserNameInputContainer>
-    </Container>)
+    <UserNameInputBackground>
+      <Wrapper>
+        <Header />
+        <UserNameInputContainer onSubmit={(event) => onFormSubmit(event)}>
+          <label htmlFor="user-input">
+            Enter your name to start the game:
+            <input
+              id="user-input"
+              type="text"
+              value={userNameInputValue}
+              required
+              // eslint-disable-next-line jsx-a11y/no-autofocus
+              autoFocus
+              onChange={(event) => setUserNameInputValue(event.target.value)}
+              placeholder="Name..." />
+          </label>
+          <StartButton type="submit">Start game</StartButton>
+        </UserNameInputContainer>
+      </Wrapper>
+    </UserNameInputBackground>
+  )
 }
 
 export default UserNameInput;
-
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-`
 
 const UserNameInputContainer = styled.form`
 border: solid 2px blue;
 display: flex;
 flex-direction: column;
 justify-content: center;
-
 `
 
 const StartButton = styled.button`
@@ -61,4 +69,8 @@ padding: 0.25em 1em;
 border: 2px solid palevioletred;
 border-radius: 3px;
 // width: 30px;
+`
+
+const UserNameInputBackground = styled.div`
+  background-color: blue;
 `
