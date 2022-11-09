584d7b6fcdc32274d2ded1a50d531bfe223cad72
Charlotte Johansson <charlotte@verismohr.com>
2022-11-09 14:57:11 +0100

    added style components, clean code

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import games, {GenerateQuestion} from '../reducers/games'
+import styled from 'styled-components'
+
 
 const GameScreen = () => {
     const description = useSelector(store => store.games.description)
@@ -22,14 +24,47 @@ const GameScreen = () => {
 
     return (
         <section>
+        <RestartButton
+        type="submit" 
+        onClick={onRestartButton}>
+            Restart
+        </RestartButton>
+        <Wrapper>
             <p>{description}</p>
             <p>{coordinates}</p>
+
             {moves && moves.map(move => (
-            <button type="submit" value={move.direction} onClick={(event) => onChooseDirection(event)}>Move {move.direction}</button>
-            ))}
-            <button type="submit" onClick={onRestartButton}>Restart</button>
-            </section>
+        <InnerContainer key={move.description}>
+             <p>{move.description}</p>
+          <Button
+            type="button"
+            value={move.direction} 
+            onClick={(event) => onChooseDirection(event)}>
+                Move {move.direction}
+        </Button>
+        </InnerContainer>
+  ))
+  }
+        </Wrapper>
+         
+           
+           </section>
     )
 }
 
 export default GameScreen
+
+const RestartButton = styled.button`
+border: solid red 2px;
+`
+const Wrapper = styled.div`
+border: solid green 2px;
+`
+const Button = styled.button`
+border: solid blue 2px;
+`
+const InnerContainer = styled.button`
+border: solid black 2px;
+`
+
+
