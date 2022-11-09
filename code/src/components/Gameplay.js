3b4d37511ec56c057ce8ed64e5956b0f0850f674
jnbjensen <mail@jnbjensen.com>
2022-11-09 17:31:15 +0100

    Loading component added

--- code/src/components/Gameplay.js
+++ code/src/components/Gameplay.js
@@ -1,20 +1,26 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/jsx-no-useless-fragment */
+
 import React from 'react';
 import { useSelector } from 'react-redux';
-
-// import { nextMove } from './reducers/game'
+import { LoadingScreen } from './LoadingScreen';
 
 export const Gameplay = () => {
   const progress = useSelector((state) => state.game.progress);
+  const isLoading = useSelector((state) => state.game.isLoading)
 
   return (
-    <div>
-      <p>Coordinates: {progress.coordinates}</p>
-      <p>Description: {progress.description}</p>
-      <p>ActionsArrayDescription: {progress.actions[0].description}</p>
-      <p>ActionsArrayDirection: {progress.actions[0].direction}</p>
-      <p>ActionsArrayType: {progress.actions[0].type}</p>
-    </div>
+    <>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <div>
+          <p>Coordinates: {progress.coordinates}</p>
+          <p>Description: {progress.description}</p>
+          <p>ActionsArrayDescription: {progress.actions[0].description}</p>
+          <p>ActionsArrayDirection: {progress.actions[0].direction}</p>
+          <p>ActionsArrayType: {progress.actions[0].type}</p>
+        </div>
+      )}
+    </>
   )
 };
