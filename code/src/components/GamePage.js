5b25e282a884691d7e5a07652ff385705d4afa4d
katirum <kartikaprw@gmail.com>
2022-11-09 14:26:33 +0100

    fixed username

--- code/src/components/GamePage.js
+++ code/src/components/GamePage.js
@@ -1,20 +1,58 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { DuckContainer } from 'styled-components/GlobalStyles';
+import { /* game, */ generateMoves } from 'reducer/game';
 import { DucklingHeader } from './DucklingHeader';
-// import { game } from 'reducer/game';
 
 export const GamePage = () => {
   const description = useSelector((store) => store.game.location);
   console.log(description)
+  const username = useSelector((store) => store.game.username);
+
+  // const dispatch = useDispatch();
+
+  // const restart = () => {
+  //   dispatch(game.actions.restart());
+  // };
+
+  // const onGenerateMoves = (direction) => {
+  //   dispatch(generateMoves(direction))
+  // }
+
+  console.log(generateMoves)
+
   return (
-    <>
+    <section>
       <DuckContainer>
         <DucklingHeader />
+        {/* console.log(G A: {generateMoves.actions})
+        {generateMoves.actions.length > 0 ? (<p>hi {username}</p>
+        ) : (
+          <p>
+            good job {username}
+          </p>
+        )} */}
+        <p>Hello: {username}</p>
       </DuckContainer>
-      <p key={description.description}>
-      DESCRIPTION: {description.description}
-      </p>
-    </>
+      <div>
+        <p key={description.description}>
+            DESCRIPTION: {description.description}
+        </p>
+      </div>
+      {/* <div>
+        {generateMoves.actions.map((option) => (
+          <div key={option.description}>
+            <p>{option.description}</p>
+            <div>
+              <button
+                type="button"
+                onClick={() => onGenerateMoves(option.direction)}>{option.type} +
+                {option.direction}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div> */}
+    </section>
   )
 }
