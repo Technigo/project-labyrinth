7435834f6cf68d63d7155ddccca1f53cddb300ae
a-well <amandaemkwellington@gmail.com>
2022-11-10 13:19:18 +0100

    Add loader

---
+++ code/src/Components/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const LoaderContainer = styled.div`
+    text-align: center;
+    padding-top: 200px;
+`
+
+const LoaderMessage = styled.p`
+    font-size: 25px;
+`
+
+const Loader = () => (
+  <LoaderContainer>
+    <LoaderMessage>Page is loading...</LoaderMessage>
+  </LoaderContainer>
+)
+
+export default Loader
+
--- code/src/Components/StartScreen.js
+++ code/src/Components/StartScreen.js
@@ -3,9 +3,12 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components'
 import Description from './Description';
 import UserNameInput from './UserNameInput';
+import Loader from './Loader';
 
 const StartScreen = () => {
   const description = useSelector((store) => store.game.description);
+
+  // if (loading) return <Loader />
 
   return (
     <StartScreenerContainer>
