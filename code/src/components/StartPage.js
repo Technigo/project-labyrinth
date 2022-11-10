dee5f1463bbd622c421b3f34da6870bc4457a986
KajaWilbik <wilbik.kaja@gmail.com>
2022-11-10 14:40:36 +0100

    after merge

--- code/src/components/StartPage.js
+++ code/src/components/StartPage.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useState } from 'react';
 import { fetchTheGame, game } from 'reducers/store';
 import { useDispatch } from 'react-redux';
-import { useUID } from 'react-uid';
 
 const StartPage = () => {
     const [userName, setUserName] = useState('')
@@ -22,17 +21,16 @@ const StartPage = () => {
             <h1> Welcome to the world of Technigo filled with magic of coding. Tell me your name and start and adventure.</h1>
             <input 
             className="userName" 
-            name={uid}
             type="text" 
             placeholder="Please write your character name"
             required
-            minlength="7" maxlength="10" size="10"
             onChange={(event) => setUserName(event.target.value)}
             value={userName} />
             <button 
             type="submit" 
             onClick={test}> Lets go </button>
        </div>
+       
     )
 
 }
