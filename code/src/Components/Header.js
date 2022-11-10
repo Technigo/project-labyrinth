de82b81d58fb28e55b8c4e0c83c77f3aff4c6cce
a-well <amandaemkwellington@gmail.com>
2022-11-10 18:51:44 +0100

    Style header

--- code/src/Components/Header.js
+++ code/src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const StyledHeader = styled.h1`
  text-align: center;
+ font-size: 2.5em;
 `
 
 const Header = () => {
   return (
-    <StyledHeader>The Maze game</StyledHeader>
+    <StyledHeader>The Maze</StyledHeader>
   )
 }
 export default Header;
