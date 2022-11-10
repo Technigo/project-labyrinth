b2b85adb6b941366ffeb200510ae9363909f994d
katirum <kartikaprw@gmail.com>
2022-11-11 00:01:08 +0100

    styling

--- code/src/styled-components/GlobalStyles.js
+++ code/src/styled-components/GlobalStyles.js
@@ -1,11 +1,47 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components/macro';
+
+export const GlobalStyles = createGlobalStyle`
+  html {
+    --pink: #fd616b;
+    --orange: #ffa46b;
+    --lightYellow: #fff6cf;
+    --purple: #683449;
+
+    font-family: 'Press Start 2P', cursive;
+    color: var(--purple);
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  h3 {
+    font-size: 1rem;
+  }
+`
 
 export const DuckContainer = styled.div`
-  height: 100px;
-  width: 100px;
-  display: flex;
-  flex-direction: column;
+  /* height: 200px;
+  width: 200px; */
+ /*  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  margin: 50px auto;
-`
+  margin: 50px 10px; */
+  /* max-width: 100%; */
+  display: grid;
+  grid-template-columns: 120px auto ;
+  align-items: center;
+  margin: 30px auto;
+  padding: 0 10% 0 10%;
+
+  p{
+    font-size: 15px;
+    line-height: 20px;
+  }
+  `
+
