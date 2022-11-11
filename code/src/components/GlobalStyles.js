617c08057b27f1f3efcfaea1e7463e90dcfbd9b9
LinneaAjger <linnea.ajger@gmail.com>
2022-11-11 15:56:48 +0100

    styled carnd and added button

--- code/src/components/GlobalStyles.js
+++ code/src/components/GlobalStyles.js
@@ -45,17 +45,64 @@ p, h1, h2, h3, h4, h5, h6 {
 
 @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');
 
+html {
+  html {
+    font-size: 16px;
+  }
+}
+
 body {
   margin: 0;
   font-family: 'JetBrains Mono', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+span {
+  display: block;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+}
+
+.instructions {
+  background-color: #323737;
+  border-radius: 10px;
+  margin-top: 1rem;
+  padding: 10px 5px;
+  width: 100%;
+  animation: fadeIn 1s; 
+}
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+.hidden-instructions {
+  display: none;
+}
+
+@media (min-width: 668px) {
+  html {
+    font-size: 20px;
+  }
+
+    }
+@media (min-width: 1025px) {
+  html {
+    font-size: 22px;
+  }
+}
+
 `
 export const Outerwrapper = styled.div`
 width: 100%;
 background-image: ${(props) =>
   (props.coordinates === '0,0' ? zeroZero : Start)};
+  height: 100%;
 `
 
 export const Innerwrapper = styled.div`
--- code/src/components/LabyrinthPart.js
+++ code/src/components/LabyrinthPart.js
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { generateActionData } from 'reducers/labyrinth'
 import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components/macro'
+import TypeIt from 'typeit-react';
 
 const LabyrinthPart = () => {
   const description = useSelector((store) => store.labyrinth.description)
   const actions = useSelector((store) => store.labyrinth.actions)
+  const [isActive, setActive] = useState('false');
 
   const dispatch = useDispatch()
 
+  const toggleDisplay = () => {
+    setActive(!isActive);
+  }
+
   return (
     <GameCard>
-      <h2>{description}</h2>
+      <TypeIt>{description}</TypeIt>
+      <button className={isActive ? null : 'instructions-btn'} type="button" onClick={toggleDisplay}>Click here to see directions</button>
       {actions.map((action) => {
         return (
-          <ActionDiv>
+          <div className={isActive ? 'hidden-instructions' : 'instructions'}>
             <p>{action.description}</p>
             <button type="button" onClick={() => dispatch(generateActionData(action.type, action.direction))}> Go {action.direction}</button>
-          </ActionDiv>
+          </div>
         )
       })}
     </GameCard>
@@ -34,6 +41,7 @@ const GameCard = styled.div`
     flex-direction: column;
     align-items: center;
     text-align: center;
+    justify-content: center;
     background-color: #222727;
     border-radius: 20px;
     color: white;
@@ -43,20 +51,22 @@ const GameCard = styled.div`
       margin-top: 1rem;
       background-color: #01FFC4;
       border: none;
-      border-radius: 6px;
+      border-radius: 5px;
+      padding: 5px 20px;
+    }
+    
+    .instructions-btn{
+    display: none;
     }
 
     @media (min-width: 668px) {
-      /* text-align: left; // Eller? */
+      padding: 1rem 2rem;
+      min-height: 400px;
+      width: 700px;
 
     }
     @media (min-width: 1025px) {
-      
+      min-height: 500px;
+      width: 800px;
     }
-`
-const ActionDiv = styled.div`
-  background-color: #323737;
-  border-radius: 10px;
-  margin-top: 1rem;
-  padding: 10px 5px;
 `
