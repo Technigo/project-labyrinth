54456b3809f1375144252bd4748b22664dbafee1
Cecilia Råden <you@example.com>
2022-11-11 14:11:09 +0100

    trying to deploy

--- code/src/components/LoadingIndicator.js
+++ code/src/components/LoadingIndicator.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 
 export const LoadingIndicator = () => {
