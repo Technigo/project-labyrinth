e814c93b380962530d782eb25a0b6f27258e960a
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2022-11-11 10:29:55 +0100

    uninstalled lottie

--- code/src/components/GameStart.js
+++ code/src/components/GameStart.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { gameState, fetchGame } from 'reducers/gamestate';
+import styled from 'styled-components/macro'
 
 const GameStart = () => {
   const dispatch = useDispatch()
@@ -13,7 +14,7 @@ const GameStart = () => {
   }
 
   return (
-    <form onSubmit={(event) => onNameSubmit(event)}>
+    <FormStyled onSubmit={(event) => onNameSubmit(event)}>
       <h1>Welcome to this labyrinth</h1>
       <label htmlFor="user-name"><h2>Please enter your name</h2>
         <input
@@ -28,8 +29,22 @@ const GameStart = () => {
         className="start-button">
         Start playing
       </button>
-    </form>
+    </FormStyled>
   )
 };
 
 export default GameStart;
+
+const FormStyled = styled.form`
+display: flex;
+justify-content: center;
+  background-size: cover;
+  background-position: 90% 10%;
+  color: white;
+  box-sizing: border-box;
+  min-height: 100vh;
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0.177),
+      rgba(250, 244, 244, 0.598)
+    ), url('https://images.unsplash.com/photo-1600758780546-7878338bb498?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjgxNTczNjA&ixlib=rb-4.0.3&q=80');
+`
