e64daa9f1bdbba3333ce1f9636f71a441e6c593e
Petra Eriksson <eriksson.petra@gmail.com>
2022-11-12 19:57:33 +0100

    Found way to solve navigation without empty divs

--- code/src/Components/GameNavigation.js
+++ code/src/Components/GameNavigation.js
@@ -48,23 +48,18 @@ const GameNavigation = ({ actions }) => {
     <ChooseDirectionWrapper>
       <p style={{ fontWeight: 'bold' }}>Choose direction</p>
       <DirectionGrid>
-        <div />
-        <div>
+        <div className="north">
           <DirectionButton direction="North" isAvailable={availableMoves.North} handleOnClick={handleOnClick} />
         </div>
-        <div />
-        <div>
+        <div className="west" >
           <DirectionButton direction="West" isAvailable={availableMoves.West} handleOnClick={handleOnClick} />
         </div>
-        <div />
-        <div>
+        <div className="east">
           <DirectionButton direction="East" isAvailable={availableMoves.East} handleOnClick={handleOnClick} />
         </div>
-        <div />
-        <div>
+        <div className="south">
           <DirectionButton direction="South" isAvailable={availableMoves.South} handleOnClick={handleOnClick} />
         </div>
-        <div />
       </DirectionGrid>
     </ChooseDirectionWrapper>
   )
@@ -80,15 +75,26 @@ const ChooseDirectionWrapper = styled.div`
 const DirectionGrid = styled.div`
     display: grid;
     grid-template-areas: 
-      "empty north empty"
-      "west empty east"
-      "empty south empty";
-    grid-template-rows: [row1-start] repeat(3, 1fr) [row1-end row2-start] repeat(3, 1fr) [row2-end row3-start] repeat(3, 1fr) [row3-end];
-    grid-template-columns: repeat(3, 1fr);    
+      ". north ."
+      "west . east"
+      ". south .";
     width: 170px;
     height: 170px;
     gap: 5px;
     justify-self: center;
+
+    .north {
+      grid-area: north;
+    }
+    .east {
+      grid-area: east;
+    }
+    .south {
+      grid-area: south;
+    }
+    .west {
+      grid-area: west;
+    }
   `
 
 export default GameNavigation;
