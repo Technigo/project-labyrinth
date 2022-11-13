5466081b778345218d402d279a54e9e58f34db77
a-well <amandaemkwellington@gmail.com>
2022-11-13 18:14:25 +0100

    Use image file in assets, clean up styling of img

--- code/src/Components/MazeLogo.js
+++ code/src/Components/MazeLogo.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-// import maze from './assets/images/maze_icon.svg';
-import maze from './maze_icon.svg';
 
 const StyledMazeLogo = styled.img`
- text-align: center;
- align-self: center;
  margin-top: 180px;
  width: 150px;
  margin-bottom: 60px;
@@ -13,7 +9,7 @@ const StyledMazeLogo = styled.img`
 
 const MazeLogo = () => {
   return (
-    <StyledMazeLogo src={maze} alt="the maze" />
+    <StyledMazeLogo src="./assets/images/maze_icon.svg" alt="the maze" />
   )
 }
 export default MazeLogo;
