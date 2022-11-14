add5842200280e1d21bb40fa1ac18deb4af7cb80
Julia Ostedt <julia.ostedt@gmail.com>
2022-11-14 10:15:40 +0100

    added new Date to username for uniqueness

--- code/src/components/StartScreen.js
+++ code/src/components/StartScreen.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/jsx-no-useless-fragment */
-/* eslint-disable max-len */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import MazePage from './MazePage';
