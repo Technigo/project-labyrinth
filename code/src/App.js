0a060078ed9a736d86a9e70043a6f26910ebb752
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-04-25 10:27:02 +0200

    now without the cat

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { Player } from '@lottiefiles/react-lottie-player';
 import fetchGame from './reducers/gameFetch'
 
 export const App = () => {
@@ -11,11 +10,7 @@ export const App = () => {
   const store = configureStore({ reducer });
   return (
     <Provider store={store}>
-      <Player
-        src="https://assets4.lottiefiles.com/packages/lf20_l9zbnyau.json"
-        className="player"
-        loop
-        autoplay />
+
       Find me in src/app.js!
     </Provider>
   )
