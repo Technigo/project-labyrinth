0a91f8245afb3f645b4d78d15f7b3aba5e1caa5f
Vio Szabo <vio@vioszabo.se>
2023-04-25 12:10:10 +0200

    endpoints just so we can see them

---
+++ code/src/Endpoints.js
@@ -0,0 +1,113 @@
+export const rooms = {
+  1: {
+    coordinates: '0,0',
+    description:
+      'You find yourself under a large archway opening into a cavern. A sense of purpose fills you.',
+    actions: [
+      {
+        type: 'move',
+        direction: 'East',
+        description:
+          "You see a worn sign that says 'The Temple of *ech*igo'. Some of the letters are missing. An overgrown paved path leads to the East",
+      },
+    ],
+  },
+  2: {
+    coordinates: '1,0',
+    description: "It's warmer here. Rays of light penetrate the ceiling.",
+    actions: [
+      {
+        type: 'move',
+        direction: 'North',
+        description:
+          "A rickety bridge crosses a large cavern to the north. Strange symbols '<' '>' '/' are engraved on the planks of the bridge.",
+      },
+      {
+        type: 'move',
+        direction: 'West',
+        description: 'You see a large archway in the distance.',
+      },
+    ],
+  },
+  3: {
+    coordinates: '1,1',
+    description:
+      'The walls of the cavern seem to grow brighter and more colorful here.',
+    actions: [
+      {
+        type: 'move',
+        direction: 'South',
+        description:
+          'The bridge somehow looks sturdier now. Seeing it fills you with confidence.',
+      },
+      {
+        type: 'move',
+        direction: 'West',
+        description:
+          'A clear colorful pattern now marks the clearly cut stone pavers leading to the West',
+      },
+    ],
+  },
+  4: {
+    coordinates: '0,1',
+    description:
+      'You emerge into a large, four-walled room with mechanical gadgets and bronze metal gizmos laying in a mess on the floor. They seem to have once been part of some larger moving mechanism.',
+    actions: [
+      {
+        type: 'move',
+        direction: 'North',
+        description:
+          'You hear faint sounds -- clicks and buzzing mechanical noises echoing on the other side of a door.',
+      },
+      {
+        type: 'move',
+        direction: 'East',
+        description:
+          'The colorful pathway to the East puts your mind at ease. You seem to understand the intention of the one who created it.',
+      },
+    ],
+  },
+  5: {
+    coordinates: '0,2',
+    description:
+      'As you move through the room, the walls change colors with every step you take. The buzzing and clicking from before now seem to form one harmonious melody. They respond to your every movement.',
+    actions: [
+      {
+        type: 'move',
+        direction: 'South',
+        description:
+          'Through the door you see that the gizmos, previously scattered on the floor, have formed one complete, whirring machine.',
+      },
+      {
+        type: 'move',
+        direction: 'North',
+        description:
+          'Looking to the North, you see an arch leading to a room containing a large chest. Papers and scrolls are stuck in the mouth of the ornate container, as if someone stored them in a hurry. Empty bookshelves line the room.',
+      },
+    ],
+  },
+  6: {
+    coordinates: '0,3',
+    description:
+      'The mouth of the chest swings open. The papers spring from the chest, whirling around your head. You shield your face, but an instant later all is calm. The papers, books and scrolls from the chest are now neatly arranged on the bookshelves around the room.',
+    actions: [
+      {
+        type: 'move',
+        direction: 'South',
+        description:
+          'A calm, faint melody flows into the room from the archway to the South. Colors bounce calmly through the threshold.',
+      },
+      {
+        type: 'move',
+        direction: 'East',
+        description:
+          'You see a bright light through an opening in the wall to the east. ',
+      },
+    ],
+  },
+  7: {
+    coordinates: '1,3',
+    description:
+      'As you step into the light, you feel an immense relief and calm flow over you. It feels like an important journey has ended, but you get the sense that a new one is just beginning...',
+    actions: [],
+  }
