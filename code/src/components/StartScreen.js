dda58430182e8849c321020f8d469ea4331e241d
OscarSindemark <oscar.sindemark@gmail.com>
2023-04-25 03:10:51 -0700

    Half of code-along code

---
+++ code/src/components/StartScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const StartScreen = () => {
+  return <div>Start Start</div>
+}
+
+export default StartScreen
+
---
+++ code/src/reducers/joke.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const jokes = createSlice({
+  name: 'dailies',
+  initialState: {
+    username: ''
+    gameStep: {
+        coordinates: "0,0"
+    }
+    joke: {},
+    category: ''
+  },
+  reducers: {
+    setJoke: (store, action) => {
+      store.joke = action.payload
+    }
+  }
+})
+
+export default jokes;
+
+const getJoke = () => {
+  return (dispatch, getState) => {
+    fetch('https://v2.jokeapi.dev/joke/Any')
+      .then((response) => response.json())
+      .then((joke) => dispatch(jokes.actions.setJoke(joke)))
+  }
+}
+
+setJoke = () => {
+
+}
