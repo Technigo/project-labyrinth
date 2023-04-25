eb7e07a1db671e2a361a5fe8703992f6e729af1d
Sammy <sammyolsson@gmail.com>
2023-04-25 21:44:17 +0200

    Boilerplate

--- code/src/App.js
+++ code/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-//Importera game from redcers här
-//Importera ui from reducers här
-//Importera main from components här
-//import GlobalStyle from
+// import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { Provider } from 'react-redux';
+// Importera game from redcers här
+// Importera ui from reducers här
+// Importera main from components här
+// Import GlobalStyle from
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!!
+      Gurus rule.
     </div>
   )
 }
