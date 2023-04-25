7ded37db5f18bb8657f79001be459957a0886995
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-25 11:15:02 +0200

    added some styles component

---
+++ code/src/components/GlobalStyles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const OuterWrapper = styled.div`
+background-color: beige;
+padding: 5% 5%;
+`
+
+export const InnerWrapper = styled.div`
+background-color: violet;
+padding: 5% 5%;
+border-radius: 5px;
+max-width: 600px;
+min-height: 50vh;
+margin: 0 auto;
+width: 80%;
+`
+
+export const Wrapper = styled.div`
+background-color: grey;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+`
