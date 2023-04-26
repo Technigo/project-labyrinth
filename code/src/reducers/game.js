8303d3f42b753655757e1b83df098ceb84d44ec6
Hannah Ek <ekhannahek@gmail.com>
2023-04-26 19:45:23 +0200

    got description and coordinates up

--- code/src/reducers/game.js
+++ code/src/reducers/game.js
@@ -10,6 +10,15 @@ const game = createSlice({
   reducers: {
     setUsername: (state, action) => {
       state.username = action.payload;
+    },
+    setActions: (state, action) => {
+      state.action = action.payload;
+    },
+    setDescription: (state, action) => {
+      state.description = action.payload;
+    },
+    setCordinates: (state, action) => {
+      state.cordinates = action.payload;
     }
   }
 });
@@ -27,6 +36,10 @@ export const generateGame = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-      });
+        dispatch(game.actions.setActions(data.actions));
+        dispatch(game.actions.setDescription(data.descriptions));
+        dispatch(game.actions.setCordinates(data.cordinates));
+      })
+      .catch((error) => console.error(error));
   }
 }
