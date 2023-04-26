7453c5736e0d1327ba89831bbe1a3045852f20b2
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-04-26 11:36:42 +0200

    started on background statement

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchTwo, gameFetch } from 'reducers/gameFetch'
+import { LevelCard, Description, DirectionButton, Background } from 'lib/Level'
 import { StarterPage } from './StarterPage'
 
 export const Game = () => {
@@ -14,26 +15,47 @@ export const Game = () => {
   const onRestartButton = () => {
     dispatch(gameFetch.actions.restartGame());
   };
+  /*   const CoordinateList = {
+    level1: '0,0',
+    level2: '0,1',
+    level3: '1,1',
+    level4: '1,2',
+    level5: '0,2',
+    level6: '0,3',
+    level7: '1,3'
+  }; */
+
+  const onLoadImages = () => {
+    if (coordinates === '') {
+      return '.images/start.png'
+    } else if (coordinates === '0,0') {
+      return '.images/level1.png'
+    } else if (coordinates === '0,1') {
+      return '.images/level2.png'
+    }
+  }
 
   return (
-    <div>
-      {coordinates === '' && (<StarterPage />)}
-      {coordinates === '1,3' && (<button type="button" onClick={onRestartButton}>Restart</button>)}
-      <p>{gameData.description}</p>
-      {gameActions && gameActions.map((item) => {
-        return (
-          <div key={item.direction}>
-            <p>{item.description}</p>
-            <button
-              direction={item.direction}
-              type="button"
-              onClick={() => dispatch(fetchTwo(item.direction))}>
-              {item.direction}
-            </button>
-          </div>
-        );
-      })}
-    </div>
+    <Background style={{ backgroundImage: `url(${onLoadImages})` }}>
+      <LevelCard>
+        {coordinates === '' && (<StarterPage />)}
+        {coordinates === '1,3' && (<button type="button" onClick={onRestartButton}>Restart</button>)}
+        <p>{gameData.description}</p>
+        {gameActions && gameActions.map((item) => {
+          return (
+            <div key={item.direction}>
+              <Description>{item.description}</Description>
+              <DirectionButton
+                direction={item.direction}
+                type="button"
+                onClick={() => dispatch(fetchTwo(item.direction))}>
+                {item.direction}
+              </DirectionButton>
+            </div>
+          );
+        })}
+      </LevelCard>
+    </Background>
   )
 }
 
---
+++ code/src/lib/Level.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+
+`
+export const LevelCard = styled.div`
+  border: 2px solid purple;
+  padding: 0 20px;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+export const Description = styled.p`
+  padding: 20px;
+`
+
+export const DirectionButton = styled.button`
+  background: #9747FF;
+`
