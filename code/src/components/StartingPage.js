0761d88eb56d1961df0684e581089877bf03e83e
tess.aquilon <tess.aquilon@gmail.com>
2023-04-26 11:19:32 +0200

    started working on a styled starting page

--- code/src/components/StartingPage.js
+++ code/src/components/StartingPage.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { game, startGame } from 'reducers/game';
+import styled from 'styled-components/macro';
+import startImage from '../assets/start.jpg'
 
 export const StartingPage = () => {
   const [username, setUsername] = useState('')
@@ -12,25 +14,68 @@ export const StartingPage = () => {
     dispatch(startGame())
   }
   return (
-    <section>
-      <div>
-        <div>
-          <p>Do you dare to enter the labyrinth cave?</p>
-          <p>Pay attention or you be lost forever</p>
-          <p>Enter your name and start your journey into the unknown</p>
-        </div>
-        <form onSubmit={onFormSubmit}>
-          <label htmlFor="usernameInput">
-            <input
-              id="usernameInput"
-              type="text"
-              placeholder="Enter your username"
-              onChange={(event) => setUsername(event.target.value)}
-              required />
-          </label>
-          <button type="submit">Next</button>
-        </form>
-      </div>
-    </section>
+    <MainSection>
+      <InnerWrapper>
+        <TextWrapper>
+          <Title>Welcome</Title>
+          <Text>Do you dare to enter the labyrinth cave?</Text>
+          <Text>Pay attention or you will be lost forever</Text>
+          <Text>Enter your name and start your journey into the unknown</Text>
+        </TextWrapper>
+        <FormWrapper>
+          <FormStyled onSubmit={onFormSubmit}>
+            <label htmlFor="usernameInput">
+              <input
+                id="usernameInput"
+                type="text"
+                placeholder="Enter your username"
+                onChange={(event) => setUsername(event.target.value)}
+                required />
+            </label>
+            <button type="submit">Start</button>
+          </FormStyled>
+        </FormWrapper>
+      </InnerWrapper>
+    </MainSection>
   )
 }
+
+const MainSection = styled.section`
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url(${startImage});
+`
+const InnerWrapper = styled.div`
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+`
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(0,0,0,.5);
+  padding: 15px;
+`
+
+const Title = styled.h1`
+color: #7D458C;
+font-size: 53px;
+`
+
+const Text = styled.p`
+color: #BFBFBF;
+`
+
+const FormWrapper = styled.div`
+background-color: rgba(0,0,0,.5);
+`
+const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+`
