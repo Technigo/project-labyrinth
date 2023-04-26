a35420d1b530cd89be14de6cc8299f13a1f1412f
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-04-26 19:00:03 +0200

    Add Header component in Player input

--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import games from 'reducers/games';
-import { StartGame } from './StartGame';
+import StartGame from './StartGame';
 
 // import GameQuestions from './GameQuestions';
 // import EndGame from './EndGame';
