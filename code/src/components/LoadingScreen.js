a758696a9f8414ceca180a9acf5414cf730a747e
jenster5 <120317167+jenster5@users.noreply.github.com>
2023-04-26 20:49:48 +0200

    trying to get lottie to work

--- code/src/components/LoadingScreen.js
+++ code/src/components/LoadingScreen.js
@@ -1,23 +1,45 @@
-import React from 'react';
-// import Lottie from 'react-lottie';
+import React, { useEffect, useRef } from 'react';
+import Lottie from 'lottie-web';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 
-const LoadingScreen = () => {
-  const isLoading = useSelector((store) => store.ui.isLoading)
+export const LoadingScreen = () => {
+  const isLoading = useSelector((store) => store.ui.isLoading);
+  const container = useRef(null);
+
+  useEffect(() => {
+    console.log('lottie is bieng loaded')
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'json',
+      loop: true,
+      autoplay: true,
+      animationData: './Lotties/lottie.json'
+      // maybe could also use url = https://lottiefiles.com/28400-hora-del-camping
+    });
+    console.log('animation object');
+  }, []);
+
+  console.log('isLoading', isLoading)
 
   return (
     <LoadingWrapper>
-      {isLoading && (
-        <><script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" /><lottie-player src="/Lotties/lottie.json" background="transparent" speed="1" loop controls autoplay /></>
-      )}
-    </LoadingWrapper>)
-}
+      <div className="container" ref={container}>
+        {isLoading && (
+          <Lottie />
+        )}
+      </div>
 
-export default LoadingScreen;
+    </LoadingWrapper>)
+};
 
 const LoadingWrapper = styled.section`
 width: 300px; 
 height: 300px;
 display:flex;
+
+.container{ 
+  width: 300px;
+  height: 300px;
+}
 `
