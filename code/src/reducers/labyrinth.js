9c52df7342f4aa11613735ace0457a80a1818263
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-04-26 09:48:49 +0200

    Cleaned up some comments

--- code/src/reducers/labyrinth.js
+++ code/src/reducers/labyrinth.js
@@ -4,10 +4,6 @@ import { loading } from './loading'
 const initialState = {
   username: null,
   currentStep: {}
-  // coordinates: '',
-  // description: '',
-  // direction: '',
-  // actions: [],
 }
 export const labyrinth = createSlice({
   name: 'labyrinth',
@@ -17,15 +13,7 @@ export const labyrinth = createSlice({
     setUsername: (store, action) => {
       store.username = action.payload
     },
-    // setCoordinates: (store, action) => {
-    //   store.coordinates = action.payload
-    // },
-    // setDescription: (store, action) => {
-    //   store.description = action.payload
-    // },
-    // setActionOption: (store, action) => {
-    //   store.actions = action.payload
-    // },
+
     setCurrentStep: (store, action) => {
       store.currentStep = action.payload
     },
@@ -71,8 +59,6 @@ export const continueGame = (direction) => {
     fetch('https://labyrinth.technigo.io/action', options)
       .then((respons) => respons.json())
       .then((data) => {
-        // dispatch(labyrinth.actions.setCoordinates(data.coordinates));
-        // dispatch(labyrinth.actions.setDescription(data.description));
         dispatch(labyrinth.actions.setCurrentStep(data));
         dispatch(loading.actions.setLoading(false));
       })
