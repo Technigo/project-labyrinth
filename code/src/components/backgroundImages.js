183e6b2dcfecc287f334c6eed54cd2c9262e6121
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-26 19:29:40 +0200

    imgs working

--- code/src/components/backgroundImages.js
+++ code/src/components/backgroundImages.js
@@ -2,7 +2,11 @@
 const backgroundImages = {
   '0,0': '/assets/img0.jpg',
   '1,0': '/assets/img0.jpg',
-  '2,0': 'assets/img0.jpg'
+  '1,1': 'assets/img0.jpg',
+  '0,1': 'assets/img0.jpg',
+  '0,2': 'assets/img0.jpg',
+  '0,3': 'assets/img0.jpg',
+  '1,3': 'assets/img0.jpg'
 };
 
 export default backgroundImages;
