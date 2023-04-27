1494a031b5c24f9e5f3c46cf6c4cfab6b57ea21e
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-27 21:47:53 +0200

    center

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -39,7 +39,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100vh;
+  width: 100%;
   height: 100vh;
   padding: 20px;
   box-sizing: border-box;
