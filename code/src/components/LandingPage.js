e0f4c2adef63fced3ce22a2a4b765e53f4fe4ee8
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-27 18:18:17 +0200

    img on landing page

--- code/src/components/LandingPage.js
+++ code/src/components/LandingPage.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  background-image: url('/assets/img0.jpg');
+  background-size: cover;
+    background-position: center;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+`;
 
 const LandingPage = ({ onStartGame }) => {
   return (
-    <div>
+
+    <Background>
       <h1> Welcome to the Adventure! </h1>
       <p>
-            Are you ready to begin your adventure?
+         Are you ready to begin your adventure?
       </p>
       <button type="button" onClick={onStartGame}>
        Start Game
       </button>
-    </div>
+    </Background>
+
   )
 }
 export default LandingPage;
