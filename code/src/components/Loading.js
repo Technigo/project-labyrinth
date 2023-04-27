df3a0349e39b3b0cab1ed09226b97014f2598ee1
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-04-27 13:59:24 +0200

    Loading animation fixed

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,34 +1,5 @@
 import React from 'react';
 import Lottie from 'lottie-react';
-import styled from 'styled-components/macro';
-import animationData from '../Lotties/retro-console-run.json';
+import Blood from '../Lotties/blood';
 
-const Loading = () => {
-  console.log(Loading)
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  }
-
-  return (
-    <LoadingPageWrapper>
-      <Lottie
-        animationdata={animationData}
-        options={defaultOptions}
-        height={100} />
-    </LoadingPageWrapper>
-
-  )
-}
-
-export default Loading;
-
-const LoadingPageWrapper = styled.div`
-display: flex; 
-flex-direction: column; 
-width: 100%; 
-`
+export const Loading = () => <Lottie style={{ width: '900px', height: '900px' }} animationData={Blood} loop />
