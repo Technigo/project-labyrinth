c156b675e8e04a8d294538adc8bee4e5c855bb45
AntoniaGranit <antonia.granit@gmail.com>
2023-04-27 10:32:01 +0300

    reinstall styled components in correct folder

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const LoadingText = styled.h1`
+  color: green;
+  font-family: "Press Start 2P";
+  font-size: 20px;
+  text-align: center;
+`
 
 export const Loading = () => {
   return (
-    <div>
-      <h1>LOADING...</h1>
-    </div>
+    <LoadingText>LOADING...</LoadingText>
   )
 }
