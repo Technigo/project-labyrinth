96734578aa9144176048467b65821079698f5cd4
Sandra Olsson <saol3165@win.su.se>
2023-04-27 18:21:41 +0200

    styling in Directions now works

--- code/src/components/Directions.js
+++ code/src/components/Directions.js
@@ -14,65 +14,34 @@ const Directions = () => {
   }
 
   return (
-    <> 
     <Direction>
       <DirectionBtn1 className={isActive ? null : 'instructions-btn'} type="button" onClick={toggleDisplay}>Click here to see directions</DirectionBtn1>
       {actions.map((action) => {
         return (
           <div>
-          <DirectionText className={isActive ? 'hidden-instructions' : 'instructions'}>
-            {action.description}</DirectionText>
+            <DirectionText className={isActive ? 'hidden-instructions' : 'instructions'}>
+              {action.description}
+            </DirectionText>
             <DirectionBtn2 type="button" key={action.direction} onClick={() => dispatch(getAction(action.type, action.direction))}> Go {action.direction}</DirectionBtn2>
-            </div>
+          </div>
         )
       })}
     </Direction>
-    </>
   )
 }
 
-export default Directions
+export default Directions;
 
 const Direction = styled.div`
-// flex-direction: column;
-
-
-const DirectionText = styled.div`
-
-&.instructions {
-  border-radius: 10px;
-  margin-top: 1rem;
-  padding: 10px 5px;
-  width: 100%;
-  animation: fadeIn 1s;
-}
-
-@keyframes fadeIn {
-  0% { opacity: 0; }
-  100% { opacity: 1; }
-}
-
-&.hidden-instructions {
-  display: none;
-}
-
-.p {
-  overflow-wrap: break-word;
-}
+flex-direction: column;
 `
-const DirectionBtn1 = styled.button`
-background: blue;
-color: white;
-padding: 10px 20px;
-border-radius: 5px;
-border: none;
-margin-right: 10px;
-margin-top: 10px;
-cursor: pointer;
-transition: 0.3s ease-in-out;
 
-&:hover {
-  transform: scale(1.05);
-  background: darkblue;
-}
+const DirectionBtn1 = styled.button`
+color: purple;
+`
+const DirectionText = styled.div`
+flex-direction: column;
+`
+const DirectionBtn2 = styled.button`
+color: yellow;
 `
