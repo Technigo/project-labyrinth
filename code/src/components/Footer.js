2f85cb4c6ad84728c5c16db62feb17ad8fc795ad
Andrea <andrea@Andreas-Air.lan>
2023-04-27 20:40:13 +0200

    footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -4,24 +4,27 @@ import styled from 'styled-components/macro';
 export const Footer = () => {
   return (
     <FooterSection>
-      <StyledA href="https://www.linkedin.com/in/emiliasaberski/">Emilia
-        <i className="fa-brands fa-linkedin-in" />
-      </StyledA>
-      <StyledA href="https://www.linkedin.com/in/fridanordenlow/">Frida
-        <i className="fa-brands fa-linkedin-in" />
-      </StyledA>
-      <StyledA href="https://www.linkedin.com/in/andrea-hedstr%C3%B6m-3549a516b/">Andrea
-        <i className="fa-brands fa-linkedin-in" />
-      </StyledA>
+      <FooterText>Designed by students @ Technigo Bootcamp</FooterText>
+      <Links>
+        <StyledA href="https://www.linkedin.com/in/emiliasaberski/">Emilia
+          <i className="fa-brands fa-linkedin-in" />
+        </StyledA>
+        <StyledA href="https://www.linkedin.com/in/fridanordenlow/">Frida
+          <i className="fa-brands fa-linkedin-in" />
+        </StyledA>
+        <StyledA href="https://www.linkedin.com/in/andrea-hedstr%C3%B6m-3549a516b/">Andrea
+          <i className="fa-brands fa-linkedin-in" />
+        </StyledA>
+      </Links>
     </FooterSection>
   )
 }
 
 const FooterSection = styled.div`
-  font-size: 17px;
+  font-size: 15px;
   position: absolute;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
@@ -32,14 +35,24 @@ const FooterSection = styled.div`
   margin-bottom: 10px;
   height: 2.5rem;
 `
+const FooterText = styled.div`
+font-size: 14px;
+color: grey;
+margin-bottom: -20px;
+`
 
 const StyledA = styled.a`
   text-decoration: none;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-right: 20px;
   color: grey;
   &:hover {
     color: white;
   }
+`
+const Links = styled.div`
+display: flex;
+flex-direction: row;
 `
