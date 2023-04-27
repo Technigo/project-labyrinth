b5636bb06b7cb80e6379312ff9c6f60432c514ea
Vio Szabo <vio@vioszabo.se>
2023-04-27 12:50:01 +0200

    backgrounds rendering on location

--- code/src/components/Backgrounds.js
+++ code/src/components/Backgrounds.js
@@ -3,38 +3,42 @@ import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import StartImg from '../images/StartPage.jpg'
 import plank from '../images/plank.jpg'
-// import light from '../images/light.jpg'
+import light from '../images/light.jpg'
 import bridge from '../images/bridge.png'
-// import ship from '../images/ship.jpg'
-// import workshop from '../images/workshop.jpg'
-// import library from '../images/library.jpg'
+import ship from '../images/ship.jpg'
+import workshop from '../images/workshop.jpg'
+import library from '../images/library.jpg'
 
 const StyledBackground = styled.div`
-background: url(${((props) => props.backgroundSwitcher)});
+background-image: ${(props) => `url(${props.backgroundImage})`};
 background-size: cover;
 background-position: center;
 `
 
 export const Backgrounds = () => {
-  const currentLocation = useSelector((store) => store.maze)
+  const currentLocation = useSelector((store) => store.maze.coordinates)
 
-  const backgroundSwitcher = () => {
-    switch (currentLocation.coordinates) {
-      case '': return '../images/StartPage.jpg'
-      case '0,0': return StartImg;
-      case '0,1': return bridge;
-      case '0,2': return '../images/library.jpg'
-      case '0,3': return '../images/light.jpg';
-      case '1,0': return '../images/light.jpg';
-      case '1,1': return plank;
-      case '1,3': return '../images/light.jpg';
-      default: return StartImg;
-    }
+  let backgroundImage;
+  switch (currentLocation) {
+    case '': return '../images/StartPage.jpg'
+    case '0,0': backgroundImage = StartImg;
+      break;
+    case '0,1': backgroundImage = bridge;
+      break;
+    case '0,2': backgroundImage = library;
+      break;
+    case '0,3': backgroundImage = light;
+      break;
+    case '1,0': backgroundImage = workshop;
+      break;
+    case '1,1': backgroundImage = plank;
+      break;
+    case '1,3': backgroundImage = ship;
+      break;
+    default: backgroundImage = ship;
   }
+
   return (
-    <div>
-      <StyledBackground background={backgroundSwitcher()} />
-      <img src="../images/StartPage.jpg" alt="" />
-    </div>
+    <StyledBackground backgroundImage={backgroundImage} />
   )
 }
--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Backgrounds } from 'components/Backgrounds'
 import styled from 'styled-components'
 import Location from './Location'
 import { Start } from './Start'
+import StartImg from '../images/StartPage.jpg'
+import light from '../images/light.jpg'
+import bridge from '../images/bridge.png'
+import ship from '../images/ship.jpg'
+import workshop from '../images/workshop.jpg'
+import library from '../images/library.jpg'
 
 const Main = () => {
   const username = useSelector((store) => store.maze.username)
+  const currentLocation = useSelector((store) => store.maze.coordinates)
+
+  let backgroundImage;
+  switch (currentLocation) {
+    case '0,0': backgroundImage = StartImg;
+      break;
+    case '0,1': backgroundImage = workshop;
+      break;
+    case '0,2': backgroundImage = library;
+      break;
+    case '0,3': backgroundImage = light;
+      break;
+    case '1,0': backgroundImage = bridge;
+      break;
+    case '1,1': backgroundImage = light;
+      break;
+    case '1,3': backgroundImage = ship;
+      break;
+    default: backgroundImage = StartImg;
+  }
   return (
-    <MainSection>
-      <Backgrounds />
+    <MainSection backgroundImage={backgroundImage}>
       {username === '' ? <Start /> : <Location />}
     </MainSection>
   )
@@ -22,5 +46,7 @@ display: flex;
 justify-content: center;
 border: pink solid 4px;
 height: 100vh;
-
+background-image: ${(props) => `url(${props.backgroundImage})`};
+background-size: cover;
+background-position: center;
 `
