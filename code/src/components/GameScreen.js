5c21e405b72d816ccecbee958ae2b327ef65ce23
Sara Battilotti <sbattigliocchi@MBPdiSbgliocchi.homenet.telecomitalia.it>
2023-04-27 15:20:28 +0200

    all good but no image working

--- code/src/components/GameScreen.js
+++ code/src/components/GameScreen.js
@@ -6,40 +6,6 @@ import { EndGame } from './EndGame';
 import { DirectionButton } from '../styles/Button';
 import { GameScreenSection, GameSection } from '../styles/Sections';
 
-/*
-export const GameScreenSection = styled.section`
-background-color: green;
-background-image: url(${maze});
-height: 100vh;
-display: flex;
-flex-direction: column;
-gap: 3%;
-align-items: center;
-justify-content: center;
-`
-export const GameSection = styled.section`
-display: flex;
-flex-direction: column;
-gap: 10px;
-font-family: "Press Start 2P";
-font-size: 10px;
-font-size: 1em;
-line-height: 1.6em;
-background: white;
-border: 10px solid black;
-align-self: center;
-width: 60%;
-margin-right: auto;
-margin-left: auto;
-padding: 2%;
-margin-bottom: 2%;
-
-@media (min-width: 770px) {
-    font-size: 1em;
-    line-height: 1.6em;
-}`
-
-*/
 export const GameScreen = () => {
   const dispatch = useDispatch();
   const gameStep = useSelector((store) => store.game.response);
