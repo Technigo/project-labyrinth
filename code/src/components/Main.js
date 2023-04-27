cd5f276d20edd9d0fbf0de45f68b687d50418e74
Vio Szabo <vio@vioszabo.se>
2023-04-27 20:12:46 +0200

    consistent names of background images

--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -4,12 +4,12 @@ import styled from 'styled-components'
 import Location from './Location'
 import { Start } from './Start'
 import StartImg from '../images/StartPage.jpg'
-import light from '../images/light.jpg'
-import entrance from '../images/entrance.jpg'
-import color from '../images/color.jpg'
-import bridge from '../images/bridge.png'
+import LightImg from '../images/light.jpg'
+import FirstImg from '../images/entrance.jpg'
+import ColorImg from '../images/color.jpg'
+import BridgeImg from '../images/bridge.png'
 import EndImg from '../images/end.jpg'
-import workshop from '../images/workshop.jpg'
+import WorkshopImg from '../images/workshop.jpg'
 import library from '../images/library.jpg'
 
 const Main = () => {
@@ -18,17 +18,17 @@ const Main = () => {
 
   let backgroundImage;
   switch (currentLocation) {
-    case '0,0': backgroundImage = entrance;
+    case '0,0': backgroundImage = FirstImg;
       break;
-    case '0,1': backgroundImage = workshop;
+    case '0,1': backgroundImage = WorkshopImg;
       break;
-    case '0,2': backgroundImage = color;
+    case '0,2': backgroundImage = ColorImg;
       break;
     case '0,3': backgroundImage = library;
       break;
-    case '1,0': backgroundImage = bridge;
+    case '1,0': backgroundImage = BridgeImg;
       break;
-    case '1,1': backgroundImage = light;
+    case '1,1': backgroundImage = LightImg;
       break;
     case '1,3': backgroundImage = EndImg;
       break;
