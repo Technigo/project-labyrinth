09c7378b9d02504421675f2af1f2dced5f9843ab
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-27 17:26:00 +0200

    fixed final page

--- code/src/components/FinalPage.js
+++ code/src/components/FinalPage.js
@@ -1,16 +1,16 @@
 // eslint-disable-next-line max-len
 /* eslint-disable max-len */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux'
 
 export const FinalPage = () => {
-  const gameSteps = useSelector((store) => store.game.gameSteps)
+  const game = useSelector((store) => store.game.gameStep)
   const username = useSelector((store) => store.game.username)
 
   return (
     <>
-      <header>Here we can put a styled header with sometinng nice congratulating the user with {username}</header>
-      <p>{gameSteps.description}</p>
+      <header>Here we can put a styled header with something nice congratulating the user with {username}</header>
+      <p>{game.description}</p>
       {/* <button /> */}
     </>
   )
--- code/src/components/GameBoard.js
+++ code/src/components/GameBoard.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getGameStep } from 'reducers/game'
-// import { LoadingPage } from './LoadingPage'
 
 const GameBoard = () => {
   const game = useSelector((store) => store.game.gameStep)
-  const username = useSelector((store) => store.game.username);
   const actions = useSelector((store) => store.game.gameStep.actions);
-
+  const username = useSelector((store) => store.game.username);
   const dispatch = useDispatch()
-
-  // const loading = useSelector((store) => store.game.loading)
 
   return (
     <div>
       <p>{game.description}</p>
       <p>{game.coordinates}</p>
+      <p>Which way will you travel, {username}?</p>
       {actions && actions.map((action) => (
         <div key={actions.direction}>
           <p>{game.type}</p>
           <p>{action.description}</p>
-          <p>Pick a direction, {username}</p>
           <button
             type="submit"
             onClick={() => dispatch(getGameStep(
