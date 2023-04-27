a08b42d55c92472143b9340f38ca9799f9f093ec
Peki <112956568+akipeki@users.noreply.github.com>
2023-04-27 16:06:50 +0300

    peki's comments

--- code/src/Components/Gamepage.js
+++ code/src/Components/Gamepage.js
@@ -1,3 +1,4 @@
+// Import React and necessary Redux hooks and components
 import React from 'react';
 import { labyrinthMango, continueGame } from 'Reducers/labyrinth';
 import { useDispatch, useSelector } from 'react-redux';
@@ -5,7 +6,9 @@ import { Loader } from './Loader';
 import { GameWrapper } from '../Styles/Globalstyles';
 import { DescriptionWrapper, DirectionWrapper, RestartBtn, DirectionBtn, GameEnd } from './Gamepage.styled';
 
+// Create Gamepage component
 export const Gamepage = () => {
+  // Set up hooks for dispatch and store
   const dispatch = useDispatch()
   const description = useSelector((store) => store.labyrinthMango.description)
   const moves = useSelector((store) => store.labyrinthMango.moves)
@@ -13,26 +16,32 @@ export const Gamepage = () => {
   const name = useSelector((store) => store.labyrinthMango.name)
   const history = useSelector((store) => store.labyrinthMango.history)
 
+  // Function to restart the game
   const restartGame = () => {
     dispatch(labyrinthMango.actions.restartGame())
   }
 
+  // Function to go to the next step
   const goToNextStep = (e) => {
     dispatch(labyrinthMango.actions.setDirection(e.target.value))
     dispatch(continueGame())
   }
 
+  // Return component content
   return (
+    // Show loader if loading
     loading ? (
       <Loader />
     ) : (
       <>
         <GameWrapper>
           <DescriptionWrapper>
-            {history.lenght === 0 ? `Welcome, ${name}.` : ''}
+            {/* Display welcome message if there's no history */}
+            {history.length === 0 ? `Welcome, ${name}.` : ''}
             <p>{description}</p>
           </DescriptionWrapper>
-          <DirectionWrapper id={moves.lenght <= 1 ? 'oneMove' : 'twoMoves'}>
+          <DirectionWrapper id={moves.length <= 1 ? 'oneMove' : 'twoMoves'}>
+            {/* Map through moves and display buttons */}
             {moves && moves.map((action) => (
               <div key={action.direction}>
                 <DirectionBtn
@@ -46,8 +55,10 @@ export const Gamepage = () => {
               </div>
             ))}
           </DirectionWrapper>
-          {moves.lenght === 0 && (
-            <GameEnd>Woho! You have made it out, {name}!</GameEnd>)}
+          {/* Show win message if no moves left */}
+          {moves.length === 0 && (
+            <GameEnd>Woho! You have made it out, {name}!</GameEnd>
+          )}
         </GameWrapper>
         <RestartBtn onClick={restartGame}>Restart &#8634;</RestartBtn>
       </>
