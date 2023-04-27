ba843d8757914563d160ef4de0537a48677d2a3e
Peki <112956568+akipeki@users.noreply.github.com>
2023-04-27 16:46:04 +0300

    pekis comments labyrint

--- code/src/Reducers/labyrinth.js
+++ code/src/Reducers/labyrinth.js
@@ -1,6 +1,8 @@
+// Import necessary libraries
 import { createSlice } from '@reduxjs/toolkit';
 import { v4 as uuidv4 } from 'uuid';
 
+// Set initial state for the store
 const initialState = {
   userName: '',
   name: '',
@@ -12,11 +14,12 @@ const initialState = {
   coordinates: ''
 }
 
+// Create Redux slice
 export const labyrinthMango = createSlice({
   name: 'labyrinthMango',
   initialState,
   reducers: {
-
+    // Set user name and generate unique user ID
     setUserName: (store, action) => {
       const input = action.payload;
       const userId = uuidv4(input);
@@ -24,33 +27,40 @@ export const labyrinthMango = createSlice({
       store.name = input;
     },
 
+    // Set description in the store
     setDescription: (store, action) => {
       store.description = action.payload;
     },
 
+    // Set moves in the store
     setMoves: (store, action) => {
       store.moves = action.payload
     },
 
+    // Set direction and update history in the store
     setDirection: (store, action) => {
       store.direction = action.payload
       store.history = [...store.history, store.direction]
     },
 
+    // Set loading state in the store
     setLoading: (store, action) => {
       store.loading = action.payload
     },
 
+    // Set coordinates in the store
     setCoordinates: (store, action) => {
       store.coordinates = action.payload
     },
 
+    // Reset game by clearing user name in the store
     restartGame: (store) => {
       store.userName = ''
     }
   }
 })
 
+// Thunk to start the game
 export const startGame = () => {
   return (dispatch, getState) => {
     dispatch(labyrinthMango.actions.setLoading(true));
@@ -74,6 +84,7 @@ export const startGame = () => {
   };
 };
 
+// Thunk to continue the game
 export const continueGame = () => {
   return (dispatch, getState) => {
     dispatch(labyrinthMango.actions.setLoading(true));
