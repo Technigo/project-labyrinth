77f9cc4a790d3e2a4994fd9005f33ee048048ef9
EmiliaGA <emilia-lundmark@hotmail.com>
2023-04-28 09:23:46 +0200

    Fixed position for loader

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -23,7 +23,11 @@ const Loading = () => {
 export default Loading
 
 const Animation = styled.div`
-
+position: absolute;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
 width: 100%;
 height: 100%;
 display: flex;
