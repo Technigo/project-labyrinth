16044f3b0f31582c7a52783dd2a47ebaee4bc00b
AnnikaSonnek <annika.sonnek@gmail.com>
2023-04-28 15:33:03 +0200

    fixed styling of animation, labyrinth

--- code/src/components/StartScreenStyles.js
+++ code/src/components/StartScreenStyles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components/macro';
 import background from '../images/startscreen.png'
-import animationData from '../images/labyrinth'
 
 export const StartScreenWrapper = styled.div`
 background: url(${background});
@@ -22,22 +21,6 @@ gap: 2rem;
 }
 `
 
-export const IconContainer = styled.div`
-margin-top: 1rem;
-background-color: rgba(255,255,255,.5);
-border-radius: 50%;
-width: 100px;
-height: 100px;
-min-width: 100px;
-min-height: 100px;
-
-@media (min-width: 767px) {
-width: 200px;
-height: 200px;
-min-width: 200px;
-min-height: 200px;
-}
-`
 export const Form = styled.form`
 display: flex;
 flex-direction: column;
@@ -67,19 +50,9 @@ max-width: 35rem;
 border-radius: 2rem;
 padding: 1.5rem;
 text-align: center;
-margin: 1rem 1rem 7rem 1rem;
+color: white;
+margin: 1rem 1rem 8rem 1rem;
 `
-
-export const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  },
-  height: '100%',
-  width: '100%'
-};
 
 export const Input = styled.input`
 max-width: 320px;
@@ -105,9 +78,6 @@ font-size: 1.3rem;
 
 export const StartBtn = styled.button`
 border: none;
-min-width: 100px;
-min-height: 35px;
-
 font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 font-size: 1rem;
 display: flex;
@@ -116,6 +86,8 @@ align-items: center;
 border-radius: 20px;
 background-color: #085e1f;
 color: white;
+width: ${(props) => (props.w80 ? '80px' : '100px;')};
+margin-top: ${(props) => (props.m2rem ? '2rem;' : '')};
 
 &:hover {
     transform: scale(1.3);
