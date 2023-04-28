fd55e8f61e9ceb05ae97fc3719f5d29d6107b6d3
Fiona Klacar <fiona.klacar@gmail.com>
2023-04-28 20:06:33 +0200

    styled the footer

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -4,13 +4,14 @@ import styled from 'styled-components';
 const Footer = () => {
   return (
     <FooterWrapper>
+      <p>Created by Annika and Fiona |</p>
       <a href="https://annikalindberg-portfolio.netlify.app/">
         <FooterImage src="/images/Annika-Lindberg.jpg" className="contact-logo" alt="link to Annika's portfolio" />
       </a>
       <a href="https://fiona-klacar-portfolio.netlify.app/">
         <FooterImage src="/images/Fiona-Klacar.jpg" className="contact-logo" alt="link to Fiona's portfolio" />
       </a>
-      <p>Created by Annika and Fiona | Technigo Bootcamp Spring 2003</p>
+      <p>| Technigo Bootcamp Spring 2023</p>
     </FooterWrapper>
   )
 }
@@ -20,13 +21,23 @@ const FooterWrapper = styled.div`
   margin: auto;
   padding: 0 20px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   flex-wrap: wrap-reverse;
-  
+  background-color: rgb(138 106 148 / 80%);
+  border-top: 2px solid black;
+  opacity: 0.95;
   `
+
 const FooterImage = styled.img`
   width: 30px;
+  padding-left: 5px;
+  padding-right: 5px;
+  opacity: 0.8;
+
+  &:hover {
+    transform: scale(1.2);
+  }
   `
 
 export default Footer;
