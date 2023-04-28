14b5c5665ed8492f962dc9b91789988d11304cc3
Hannah Ek <ekhannahek@gmail.com>
2023-04-28 10:03:45 +0200

    solved the background images

--- code/src/components/GlobalStyles/Background.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-nested-ternary */
-import styled from 'styled-components';
-import { Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8 } from 'components/GlobalStyles/Images';
-
-export const Background = styled.section`
-background-image: url(${(props) => (props.coordinates === '' ? Img1 : props.coordinates === '0,0' ? Img2 : props.coordinates === '1,0' ? Img3 : props.coordinates === '1,1' ? Img4 : props.coordinates === '0,1' ? Img5 : props.coordinates === '0,2' ? Img6 : props.coordinates === '0,3' ? Img7 : props.coordinates === '1,3' ? Img8 : Img1)});
-`
--- code/src/components/Main.js
+++ code/src/components/Main.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable no-nested-ternary */
 import React from 'react';
 import { useSelector } from 'react-redux';
@@ -9,9 +8,29 @@ import { GameBoard } from './GameBoard';
 const Main = () => {
   const isLoading = useSelector((store) => store.ui.isLoading);
   const username = useSelector((store) => store.game.username);
+  const coordinates = useSelector((store) => store.game.coordinates);
+  let imageUrl;
+
+  if (coordinates === '0,0') {
+    imageUrl = 'https://i.postimg.cc/pV8sx0sd/maze-1.png';
+  } else if (coordinates === '0,1') {
+    imageUrl = 'https://i.postimg.cc/bJS3ZL83/maze-4.png';
+  } else if (coordinates === '0,2') {
+    imageUrl = 'https://i.postimg.cc/9FY1wD55/maze-5.png';
+  } else if (coordinates === '0,3') {
+    imageUrl = 'https://i.postimg.cc/RVBX0cYL/maze-6.png';
+  } else if (coordinates === '1,0') {
+    imageUrl = 'https://i.postimg.cc/52P3pD2s/maze-2.png';
+  } else if (coordinates === '1,1') {
+    imageUrl = 'https://i.postimg.cc/Gt0zGYmr/maze-3.png';
+  } else if (coordinates === '1,3') {
+    imageUrl = 'https://i.postimg.cc/Dz8cN5Wn/maze-7.png';
+  } else {
+    imageUrl = 'https://i.postimg.cc/x1j5v9nk/maze-intro.png';
+  }
 
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${imageUrl})` }}>
       {isLoading ? (
         <LoadingMaze />
       ) : username ? (
