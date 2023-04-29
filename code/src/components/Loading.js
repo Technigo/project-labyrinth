69befdbb31a7502abc3079bee9e64179a5ef5af3
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-04-29 18:39:16 +0200

    loading

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+  background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Image = styled.img`
+  filter: grayscale(100%);
+  width: 150px;
+`;
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading.....</h1>
-    </div>
+    <PageWrapper>
+      <Image src="/assets/gif.webp" alt="Loading" />
+    </PageWrapper>
   );
 };
 
