c5c82b852f203de56250d09dede50472995c6428
cecavi <100417732+cecavi@users.noreply.github.com>
2023-04-29 08:40:28 +0200

    added loading lottie

--- code/src/components/Loading.js
+++ code/src/components/Loading.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Loading = () => {
   return (
-    <p>Loading</p>
-  )
+
+    <Animation>
+      <Player
+        src="https://assets9.lottiefiles.com/packages/lf20_pa2kzq5x.json"
+        className="lottie"
+        loop
+        autoplay
+        speed={1}
+        style={{ height: '400px', width: '400px' }} />
+    </Animation>
+  );
 }
 
-export default Loading;
+export default Loading
+
+const Animation = styled.div`
+background-color: white;
+position: absolute;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+z-index: 9999;
+`
