af1a28b0dd5d5d31a8cbbfe34fad720981235869
dannebrob <65211641+dannebrob@users.noreply.github.com>
2023-05-01 15:44:37 +0200

    css fix

--- code/src/components/Game.js
+++ code/src/components/Game.js
@@ -41,13 +41,8 @@ const StyledText = styled.h2`
 `;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
   width: 100%;
   height: 100vh;
-  padding: 20px;
   box-sizing: border-box;
 `;
 
