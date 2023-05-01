d634653c1629988726d5c403990f0c5daf04bbf5
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-01 17:51:32 +0200

    Update Loading background

--- code/src/components/LoadingStyling.js
+++ code/src/components/LoadingStyling.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const LoadingContainer = styled.div`
-  background-color: rgba(145, 132, 115, 0.7);
+  background-color: rgba(145, 132, 115, 0.3);
   background-size: cover;
   background-position: center;
   display: flex;
@@ -13,16 +13,3 @@ export const LoadingContainer = styled.div`
   align-items: center;
   text-align: center;
 `;
-
-// export const LoadingWrapper = styled.div`
-//   background-color: rgba(145, 132, 115, 0.7);
-//   max-width: 80%;
-//   margin: 2rem auto;
-//   padding: 1rem;
-//   display: flex;
-//   justify-content: center;
-
-//   @media (min-width: 1440px) {
-//     max-width: 50rem;
-//   }
-// `;
