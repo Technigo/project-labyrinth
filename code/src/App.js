a31fb6ad7cfc57a2980e7dc4cdf8bfc54512f1c6
Peki <112956568+akipeki@users.noreply.github.com>
2023-05-05 11:45:20 +0300

    added an apploader

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { labyrinthMango } from './Reducers/labyrinth';
 import { Firstscreen } from './Components/Firstscreen';
 import { GlobalRules } from './Styles/Globalstyles';
+import AppLoader from './AppLoader'; // import AppLoader component
 
 export const App = () => {
+  const [loading, setLoading] = useState(true); // define loading state
+
+  useEffect(() => {
+    // set a delay of 1 second before setting loading to false
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer); // cleanup function to clear the timer
+  }, []);
+
   const reducer = combineReducers({
     labyrinthMango: labyrinthMango.reducer
   });
@@ -15,7 +27,7 @@ export const App = () => {
     <div>
       <Provider store={store}>
         <GlobalRules />
-        <Firstscreen />
+        {loading ? <AppLoader /> : <Firstscreen />}
       </Provider>
     </div>
   )
---
+++ code/src/AppLoader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AppLoader.css';
+
+const AppLoader = () => {
+  return (
+    <div className="loader-container">
+      <p className="loader">
+                .
+      </p>
+    </div>
+  );
+};
+export default AppLoader;
