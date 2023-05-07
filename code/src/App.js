2747c6d40520c63bbf094c83f29ee62b2431e909
Peki <112956568+akipeki@users.noreply.github.com>
2023-05-07 22:34:46 +0300

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { labyrinthMango } from './Reducers/labyrinth';
 import { Firstscreen } from './Components/Firstscreen';
 import { GlobalRules } from './Styles/Globalstyles';
-import AppLoader from './AppLoader'; // import AppLoader component
+// import AppLoader from './AppLoader'; // import AppLoader component
 
 export const App = () => {
-  const [loading, setLoading] = useState(true); // define loading state
+  // const [loading, setLoading] = useState(true); // define loading state
 
-  useEffect(() => {
-    // set a delay of 1 second before setting loading to false
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000);
+  //  useEffect(() => {
+  // set a delay of 1 second before setting loading to false
+  //    const timer = setTimeout(() => {
+  //      setLoading(false);
+  //    }, 1000);
 
-    return () => clearTimeout(timer); // cleanup function to clear the timer
-  }, []);
+  //    return () => clearTimeout(timer); // cleanup function to clear the timer
+  //  }, []);
 
   const reducer = combineReducers({
     labyrinthMango: labyrinthMango.reducer
@@ -27,7 +27,7 @@ export const App = () => {
     <div>
       <Provider store={store}>
         <GlobalRules />
-        {loading ? <AppLoader /> : <Firstscreen />}
+        <Firstscreen />
       </Provider>
     </div>
   )
