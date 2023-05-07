47cc54b9521a5b9fad70a830ef867d1860262a44
Peki <112956568+akipeki@users.noreply.github.com>
2023-05-07 23:04:13 +0300

    Update Loader.js Removed background image to make it work

--- code/src/Components/Loader.js
+++ code/src/Components/Loader.js
@@ -1,7 +1,8 @@
 // Import necessary libraries
 import React from 'react';
 import { useSelector } from 'react-redux'; // useSelector allows us to extract data from the Redux store state
-import { CoordsImageDisplay } from './CoordsImageDisplay'; // This is our custom component that displays the correct background image based on the current coordinates
+// import { CoordsImageDisplay } from './CoordsImageDisplay'; REMOVED
+// Our custom component that displays the correct background image based on the current coordinates
 import { GameWrapper, LoadingText } from '../Styles/Globalstyles'; // These are styled components for the game wrapper and loading text
 
 // Define the Loader component
@@ -9,7 +10,7 @@ export const Loader = () => {
   const loading = useSelector((store) => store.labyrinthMango.loading);
 
   // Get the current game's coordinates from the Redux store
-  const coordinates = useSelector((store) => store.labyrinthMango.coordinates);
+  // const coordinates = useSelector((store) => store.labyrinthMango.coordinates); REMOVED
 
   return (
     <GameWrapper>
@@ -17,8 +18,8 @@ export const Loader = () => {
       {loading && (<LoadingText>Loading.. </LoadingText>)}
       {/* Here, we are rendering the CoordsImageDisplay component with the current coordinates */}
       {/* If coordinates are not yet set, we pass a default value of '0,0' */}
-      <CoordsImageDisplay
-        coordinates={coordinates || '0,0'} />
+      {/*   <CoordsImageDisplay
+        coordinates={coordinates || '0,0'} /> REMOVED */}
     </GameWrapper>
   );
 };
