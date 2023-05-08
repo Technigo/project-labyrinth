4add30ea51f7d0d26724e75d054ab440c3b8f9c8
Peki <112956568+akipeki@users.noreply.github.com>
2023-05-08 09:25:55 +0300

    removed backggorund image from the loader to make it work

--- code/src/Components/Loader.js
+++ code/src/Components/Loader.js
@@ -1,7 +1,8 @@
 // Import necessary libraries
 import React from 'react';
 import { useSelector } from 'react-redux'; // useSelector allows us to extract data from the Redux store state
-import { CoordsImageDisplay } from './CoordsImageDisplay'; // This is our custom component that displays the correct background image based on the current coordinates
+// import { CoordsImageDisplay } from './CoordsImageDisplay'; REMOVED
+// Our custom component that displays the correct background image based on the current coordinates
 import { GameWrapper, LoadingText } from '../Styles/Globalstyles'; // These are styled components for the game wrapper and loading text
 
 // Define the Loader component
@@ -9,7 +10,7 @@ export const Loader = () => {
   const loading = useSelector((store) => store.labyrinthMango.loading);
 
   // Get the current game's coordinates from the Redux store
-  const coordinates = useSelector((store) => store.labyrinthMango.coordinates);
+  // const coordinates = useSelector((store) => store.labyrinthMango.coordinates); REMOVED
 
   return (
     <GameWrapper>
@@ -17,14 +18,8 @@ export const Loader = () => {
       {loading && (<LoadingText>Loading.. </LoadingText>)}
       {/* Here, we are rendering the CoordsImageDisplay component with the current coordinates */}
       {/* If coordinates are not yet set, we pass a default value of '0,0' */}
-      <CoordsImageDisplay
-        coordinates={coordinates || '0,0'} />
+      {/*   <CoordsImageDisplay
+        coordinates={coordinates || '0,0'} /> REMOVED */}
     </GameWrapper>
   );
 };
-
-// Finally, our Loader does two main things:
-
-// If the loaded switch is off, it shows "Loading.." message. If it's on, it doesn't show anything.
-// It calls upon the CoordsImageDisplay, that finds the right background image for the current room.
-// CoordsImageDisplay also tells handleImageLoad when the image is loaded and ready to be shown.
